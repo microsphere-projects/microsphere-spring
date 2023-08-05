b2e1376d246ea70ea04d2d94cc41538b33ce78f3
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-08-05 09:13:59 +0800

    Add getSpringVersion

--- microsphere-spring-context/src/main/java/io/microsphere/spring/util/SpringVersionUtils.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/util/SpringVersionUtils.java
@@ -16,11 +16,39 @@
  */
 package io.microsphere.spring.util;
 
+import io.microsphere.util.BaseUtils;
+import io.microsphere.util.Version;
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
+
+import static io.microsphere.text.FormatUtils.format;
+
 /**
  * The Utilities class for Spring Framework Version
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public abstract class SpringVersionUtils {
+public abstract class SpringVersionUtils extends BaseUtils {
+
+    public static final String SPRING_BASE_PACKAGE_NAME = "org.springframework";
+
+    /**
+     * Get the {@link Version} from the Spring class
+     *
+     * @param springClass the Spring class
+     * @return non-null
+     * @throws NullPointerException     if <code>springClass</code> is <code>null</code>
+     * @throws IllegalArgumentException if the package name of <code>springClass</code> does not start with "org.springframework"
+     */
+    @NonNull
+    public static Version getSpringVersion(Class<?> springClass) throws NullPointerException, IllegalArgumentException {
+        Package classPackage = springClass.getPackage();
+        String classPackageName = classPackage.getName();
+        Assert.isTrue(classPackageName.startsWith(SPRING_BASE_PACKAGE_NAME),
+                () -> format("The class[name : '{}'] is not packaged into the Spring package['{}']",
+                        springClass.getName(), SPRING_BASE_PACKAGE_NAME));
+        String implementationVersion = classPackage.getImplementationVersion();
+        return Version.of(implementationVersion);
+    }
 }
