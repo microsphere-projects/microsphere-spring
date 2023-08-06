6679af986b7da8d68ef175094c7c857185cf9f04
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-08-06 12:57:40 +0800

    Update SpringVersionUtils.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/util/SpringVersionUtils.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/util/SpringVersionUtils.java
@@ -16,22 +16,81 @@
  */
 package io.microsphere.spring.util;
 
+import io.microsphere.spring.core.SpringVersion;
 import io.microsphere.util.BaseUtils;
 import io.microsphere.util.Version;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+
 import org.springframework.lang.NonNull;
-import org.springframework.util.Assert;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
 
 import static io.microsphere.text.FormatUtils.format;
+import static io.microsphere.util.ClassLoaderUtils.getDefaultClassLoader;
+import static io.microsphere.util.ClassLoaderUtils.resolveClass;
 
 /**
  * The Utilities class for Spring Framework Version
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see SpringVersion
+ * @see org.springframework.core.SpringVersion
+ * @see Version
  * @since 1.0.0
  */
 public abstract class SpringVersionUtils extends BaseUtils {
 
+    private static final ClassLoader classLoader = getDefaultClassLoader();
+
     public static final String SPRING_BASE_PACKAGE_NAME = "org.springframework";
+
+    /**
+     * The version of "spring-core" module
+     */
+    @NonNull
+    public static final Version SPRING_CORE_VERSION = getSpringVersion(StringUtils.class);
+
+    /**
+     * The version of "spring-aop" module
+     */
+    @Nullable
+    public static final Version SPRING_AOP_VERSION = getSpringVersion("org.springframework.aop.Advisor");
+
+    /**
+     * The version of "spring-beans" module
+     */
+    @NonNull
+    public static final Version SPRING_BEANS_VERSION = getSpringVersion(BeanFactory.class);
+
+    /**
+     * The version of "spring-context" module
+     */
+    @NonNull
+
+    public static final Version SPRING_CONTEXT_VERSION = getSpringVersion(ApplicationContext.class);
+
+    /**
+     * The version of "spring-context" module
+     */
+    @Nullable
+    public static final Version SPRING_CONTEXT_SUPPORT_VERSION = getSpringVersion("org.springframework.mail.MailSender");
+
+    /**
+     * Get the {@link Version} from the Spring class
+     *
+     * @param springClassName the Spring class name
+     * @return <code>null</code> of the specified class can't be found by its' name
+     * the "Implementation-Version" attribute is absent in the "META-INF/MANIFEST.MF" resource
+     * @throws NullPointerException     if <code>springClass</code> is <code>null</code>
+     * @throws IllegalArgumentException if the package name of <code>springClass</code> does not start with "org.springframework"
+     * @throws IllegalStateException    if the "Implementation-Version" attribute is absent in the "META-INF/MANIFEST.MF" resource
+     */
+    @Nullable
+    public static Version getSpringVersion(String springClassName) throws NullPointerException, IllegalArgumentException {
+        Class<?> springClass = resolveClass(springClassName, classLoader);
+        return springClass == null ? null : getSpringVersion(springClass);
+    }
 
     /**
      * Get the {@link Version} from the Spring class
@@ -40,15 +99,20 @@ public abstract class SpringVersionUtils extends BaseUtils {
      * @return non-null
      * @throws NullPointerException     if <code>springClass</code> is <code>null</code>
      * @throws IllegalArgumentException if the package name of <code>springClass</code> does not start with "org.springframework"
+     * @throws IllegalStateException    if the "Implementation-Version" attribute is absent in the "META-INF/MANIFEST.MF" resource
      */
     @NonNull
     public static Version getSpringVersion(Class<?> springClass) throws NullPointerException, IllegalArgumentException {
         Package classPackage = springClass.getPackage();
         String classPackageName = classPackage.getName();
-        Assert.isTrue(classPackageName.startsWith(SPRING_BASE_PACKAGE_NAME),
-                () -> format("The class[name : '{}'] is not packaged into the Spring package['{}']",
-                        springClass.getName(), SPRING_BASE_PACKAGE_NAME));
+        if (!classPackageName.startsWith(SPRING_BASE_PACKAGE_NAME)) {
+            String errorMessage = format("The class[name : '{}'] is not packaged into the Spring package['{}']", springClass.getName(), SPRING_BASE_PACKAGE_NAME);
+            throw new IllegalArgumentException(errorMessage);
+        }
         String implementationVersion = classPackage.getImplementationVersion();
-        return Version.of(implementationVersion);
+        if (implementationVersion == null) {
+
+        }
+        return implementationVersion == null ? null : Version.of(implementationVersion);
     }
 }
