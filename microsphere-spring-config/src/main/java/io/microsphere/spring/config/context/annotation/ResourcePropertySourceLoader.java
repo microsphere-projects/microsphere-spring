33450033bdb02f54713500374063b081fc02360f
Mercy Ma <mercyblitz@gmail.com>
2023-09-18 21:49:08 +0800

    Update ResourcePropertySourceLoader.java

--- microsphere-spring-config/src/main/java/io/microsphere/spring/config/context/annotation/ResourcePropertySourceLoader.java
+++ microsphere-spring-config/src/main/java/io/microsphere/spring/config/context/annotation/ResourcePropertySourceLoader.java
@@ -16,12 +16,25 @@
  */
 package io.microsphere.spring.config.context.annotation;
 
+import io.microsphere.io.StandardFileWatchService;
+import io.microsphere.io.event.FileChangedEvent;
+import io.microsphere.io.event.FileChangedListener;
 import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.core.io.support.ResourcePatternUtils.getResourcePatternResolver;
@@ -34,11 +47,16 @@ import static org.springframework.core.io.support.ResourcePatternUtils.getResour
  * @see PropertySourceExtensionAttributes
  * @since 1.0.0
  */
-public class ResourcePropertySourceLoader extends PropertySourceExtensionLoader<ResourcePropertySource, PropertySourceExtensionAttributes<ResourcePropertySource>> implements ResourceLoaderAware, BeanClassLoaderAware {
+public class ResourcePropertySourceLoader extends PropertySourceExtensionLoader<ResourcePropertySource, PropertySourceExtensionAttributes<ResourcePropertySource>>
+        implements ResourceLoaderAware, BeanClassLoaderAware, DisposableBean {
 
     private ResourcePatternResolver resourcePatternResolver;
 
     private ClassLoader classLoader;
+
+    private StandardFileWatchService fileWatchService;
+
+    private PropertySourceExtensionAttributes<ResourcePropertySource> extensionAttributes;
 
     @Override
     protected Resource[] getResources(PropertySourceExtensionAttributes<ResourcePropertySource> extensionAttributes, String propertySourceName, String resourceValue)
@@ -48,9 +66,63 @@ public class ResourcePropertySourceLoader extends PropertySourceExtensionLoader<
 
     @Override
     protected void configureAutoRefreshedResources(PropertySourceExtensionAttributes<ResourcePropertySource> extensionAttributes,
-                                                   String propertySourceName, String[] resourceValues, List<Resource> resourcesList) {
+                                                   String propertySourceName, String[] resourceValues,
+                                                   List<Resource> resourcesList) throws Throwable {
+        // resource location : classpath:/META-INF/a.properties ->
+        // actual path(origin) : /users/admin/some-project/META-INF/a.properties
+        // name = a
+
+        // actual path(modified) : /users/admin/some-project/META-INF/a.properties
+        // -> name = b
+        // based on FileSystem
+        // interesting kind = MODIFIED
+
+        this.fileWatchService = new StandardFileWatchService();
+        this.extensionAttributes = extensionAttributes;
+
+        Listener listener = new Listener();
+        for (Resource resource : resourcesList) {
+            if (isFileSystemBasedResource(resource)) {
+                File resourceFile = resource.getFile();
+                fileWatchService.watch(resourceFile, listener, FileChangedEvent.Kind.MODIFIED);
+            }
+        }
+
+        fileWatchService.start();
 
     }
+
+    class Listener implements FileChangedListener {
+
+        @Override
+        public void onFileModified(FileChangedEvent event) {
+
+            // a.properties > b.properties
+            // name = a   >   name = b
+
+            // PropertySource file has been modified
+            ConfigurableEnvironment environment = getEnvironment();
+            MutablePropertySources propertySources = environment.getPropertySources();
+            File resourceFile = event.getFile();
+            String encoding = extensionAttributes.getEncoding();
+            Resource resource = new FileSystemResource(resourceFile);
+            EncodedResource encodedResource = new EncodedResource(resource, encoding);
+            PropertySourceFactory factory = createPropertySourceFactory(extensionAttributes);
+            String propertySourceName = resourceFile.getAbsolutePath();
+            try {
+                PropertySource propertySource = factory.createPropertySource(propertySourceName, encodedResource);
+                propertySources.addFirst(propertySource);
+            } catch (IOException e) {
+                logger.error("TODO message", e);
+            }
+        }
+    }
+
+    private boolean isFileSystemBasedResource(Resource resource) {
+        return resource instanceof FileSystemResource ||
+                resource instanceof FileUrlResource;
+    }
+
 
     @Override
     public void setResourceLoader(ResourceLoader resourceLoader) {
@@ -60,5 +132,12 @@ public class ResourcePropertySourceLoader extends PropertySourceExtensionLoader<
     @Override
     public void setBeanClassLoader(ClassLoader classLoader) {
         this.classLoader = classLoader;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        if (fileWatchService != null) {
+            fileWatchService.stop();
+        }
     }
 }
