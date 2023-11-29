fd41ac04e9dca4d2cabe1644dafcf17754919f26
小马哥（mercyblitz） <mercyblitz@gmail.com>
2023-11-29 17:44:23 +0800

    Create DefaultPropertiesPropertySources.java

---
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/config/context/annotation/DefaultPropertiesPropertySources.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.spring.config.context.annotation;
+
+import io.microsphere.spring.config.env.support.DefaultResourceComparator;
+import io.microsphere.spring.util.PropertySourcesUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
+import org.springframework.core.io.support.PropertySourceFactory;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Comparator;
+
+/**
+ * The annotation declaring the multiple {@link DefaultPropertiesPropertySource @DefaultPropertiesPropertySource}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see DefaultPropertiesPropertySource
+ * @see DefaultPropertiesPropertySourceLoader
+ * @see org.springframework.core.env.PropertySource
+ * @see Configuration
+ * @since 1.0.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Import(DefaultPropertiesPropertySourceLoader.class)
+public @interface DefaultPropertiesPropertySources {
+
+    DefaultPropertiesPropertySource[] value();
+}
