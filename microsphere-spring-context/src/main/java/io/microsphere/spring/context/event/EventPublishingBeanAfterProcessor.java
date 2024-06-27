db2bc4f2d5387b9231aef5d0623dace1c5f316da
Mercy Ma <mercyblitz@gmail.com>
2024-06-27 22:13:03 +0800

    Update EventPublishingBeanAfterProcessor.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/context/event/EventPublishingBeanAfterProcessor.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/context/event/EventPublishingBeanAfterProcessor.java
@@ -16,11 +16,11 @@
  */
 package io.microsphere.spring.context.event;
 
+import io.microsphere.spring.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
 import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationEvent;
