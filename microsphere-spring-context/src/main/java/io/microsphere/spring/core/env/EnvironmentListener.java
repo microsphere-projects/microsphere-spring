b31af1681927a770109fbd80b1f4ac250dd01f8b
Mercy Ma <mercyblitz@gmail.com>
2024-11-22 13:42:56 +0800

    Update EnvironmentListener.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/EnvironmentListener.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/EnvironmentListener.java
@@ -17,21 +17,83 @@
 package io.microsphere.spring.core.env;
 
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.Map;
 
 /**
- * The interface listens the manipulation of {@link ConfigurableEnvironment} is composited by {@link ProfileListener}
+ * The interface listens the manipulation of {@link ConfigurableEnvironment} including {@link ProfileListener}
+ * and {@link PropertyResolverListener} :
+ * <ul>
+ *     <li>{@link ConfigurableEnvironment#getPropertySources()}</li>
+ *     <li>{@link ConfigurableEnvironment#getSystemProperties()}</li>
+ *     <li>{@link ConfigurableEnvironment#getSystemEnvironment()}</li>
+ *     <li>{@link ConfigurableEnvironment#merge(ConfigurableEnvironment)}</li>
+ * </ul>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see ProfileListener
+ * @see PropertyResolverListener
  * @see ConfigurableEnvironment
  * @since 1.0.0
  */
 public interface EnvironmentListener extends ProfileListener, PropertyResolverListener {
 
     /**
+     * Callback before {@link ConfigurableEnvironment#getPropertySources()}
+     *
+     * @param environment {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
+     */
+    default void beforeGetPropertySources(ConfigurableEnvironment environment) {
+    }
+
+    /**
+     * Callback after {@link ConfigurableEnvironment#getPropertySources()}
+     *
+     * @param environment            {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
+     * @param propertySources {@link MutablePropertySources} was returned by {@link ConfigurableEnvironment#getPropertySources()}
+     */
+    default void afterGetPropertySources(ConfigurableEnvironment environment, MutablePropertySources propertySources) {
+    }
+
+    /**
+     * Callback before {@link ConfigurableEnvironment#getSystemProperties()}
+     *
+     * @param environment {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
+     */
+    default void beforeGetSystemProperties(ConfigurableEnvironment environment) {
+    }
+
+    /**
+     * Callback after {@link ConfigurableEnvironment#getSystemProperties()}
+     *
+     * @param environment      {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
+     * @param systemProperties the value of {@link System#getProperties()} was returned by {@link ConfigurableEnvironment#getSystemProperties()}
+     */
+    default void afterGetSystemProperties(ConfigurableEnvironment environment, Map<String, Object> systemProperties) {
+    }
+
+    /**
+     * Callback before {@link ConfigurableEnvironment#getSystemEnvironment()}
+     *
+     * @param environment {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
+     */
+    default void beforeGetSystemEnvironment(ConfigurableEnvironment environment) {
+    }
+
+    /**
+     * Callback after {@link ConfigurableEnvironment#getSystemEnvironment()}
+     *
+     * @param environment                {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
+     * @param systemEnvironmentVariables the value of {@link System#getenv()} was returned by {@link ConfigurableEnvironment#getSystemEnvironment()}
+     */
+    default void afterGetSystemEnvironment(ConfigurableEnvironment environment, Map<String, Object> systemEnvironmentVariables) {
+    }
+
+    /**
      * Callback before {@link ConfigurableEnvironment#merge(ConfigurableEnvironment)}
      *
-     * @param environment       current {@link ConfigurableEnvironment}
+     * @param environment       {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
      * @param parentEnvironment parent {@link ConfigurableEnvironment}
      */
     default void beforeMerge(ConfigurableEnvironment environment, ConfigurableEnvironment parentEnvironment) {
@@ -40,7 +102,7 @@ public interface EnvironmentListener extends ProfileListener, PropertyResolverLi
     /**
      * Callback after {@link ConfigurableEnvironment#merge(ConfigurableEnvironment)}
      *
-     * @param environment       current {@link ConfigurableEnvironment}
+     * @param environment       {@link ConfigurableEnvironment the underlying ConfigurableEnvironment}
      * @param parentEnvironment parent {@link ConfigurableEnvironment}
      */
     default void afterMerge(ConfigurableEnvironment environment, ConfigurableEnvironment parentEnvironment) {
