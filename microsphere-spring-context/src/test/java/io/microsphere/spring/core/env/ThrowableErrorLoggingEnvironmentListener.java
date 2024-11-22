efdfd71e0f7f7d184d8a364522a8f2e1b5fac3ab
Mercy Ma <mercyblitz@gmail.com>
2024-11-22 13:43:06 +0800

    Create ThrowableErrorLoggingEnvironmentListener.java

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/core/env/ThrowableErrorLoggingEnvironmentListener.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.spring.core.env;
+
+/**
+ * {@link EnvironmentListener} with random error to be thrown
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see EnvironmentListener
+ * @since 1.0.0
+ */
+public class ThrowableErrorLoggingEnvironmentListener extends LoggingEnvironmentListener {
+
+    @Override
+    protected void log(String message) {
+        throw new RuntimeException("For Testing");
+    }
+
+    @Override
+    protected void log(String messagePattern, Object... args) {
+        this.log(messagePattern);
+    }
+}
