de2e3650294bb8bebc1385e534e9b883dc8383b3
Mercy Ma <mercyblitz@gmail.com>
2024-11-22 13:42:53 +0800

    Update PropertyResolverListener.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/PropertyResolverListener.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/PropertyResolverListener.java
@@ -18,17 +18,18 @@ package io.microsphere.spring.core.env;
 
 import org.springframework.core.convert.support.ConfigurableConversionService;
 import org.springframework.core.env.ConfigurablePropertyResolver;
+import org.springframework.core.env.PropertyResolver;
 import org.springframework.lang.Nullable;
 
 /**
  * The interface listens the manipulation of {@link ConfigurablePropertyResolver PropertyResolvers'} profiles including:
  *
  * <ul>
- *     <li>{@link ConfigurablePropertyResolver#getProperty(String, String)}</li>
- *     <li>{@link ConfigurablePropertyResolver#getProperty(String, Class, Object)}</li>
- *     <li>{@link ConfigurablePropertyResolver#getRequiredProperty(String, Class)}</li>
- *     <li>{@link ConfigurablePropertyResolver#resolvePlaceholders(String)}</li>
- *     <li>{@link ConfigurablePropertyResolver#resolveRequiredPlaceholders(String)}</li>
+ *     <li>{@link PropertyResolver#getProperty(String, String)}</li>
+ *     <li>{@link PropertyResolver#getProperty(String, Class, Object)}</li>
+ *     <li>{@link PropertyResolver#getRequiredProperty(String, Class)}</li>
+ *     <li>{@link PropertyResolver#resolvePlaceholders(String)}</li>
+ *     <li>{@link PropertyResolver#resolveRequiredPlaceholders(String)}</li>
  *     <li>{@link ConfigurablePropertyResolver#setRequiredProperties(String...)}</li>
  *     <li>{@link ConfigurablePropertyResolver#validateRequiredProperties()}</li>
  *     <li>{@link ConfigurablePropertyResolver#setConversionService(ConfigurableConversionService)}</li>
@@ -45,74 +46,53 @@ import org.springframework.lang.Nullable;
 public interface PropertyResolverListener {
 
     /**
-     * Callback before {@link ConfigurablePropertyResolver#getProperty(String, String)}
+     * Callback before {@link PropertyResolver#getProperty(String, String)} or{@link PropertyResolver#getProperty(String, Class, Object)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param name             the name of the property
-     * @param defaultValue     the default value of the property
+     * @param targetType       the target type to be converted by the property
+     * @param defaultValue     the default value of the target-typed property
      */
-    default void beforeGetProperty(ConfigurablePropertyResolver propertyResolver, String name,
-                                   @Nullable String defaultValue) {
+    default void beforeGetProperty(ConfigurablePropertyResolver propertyResolver, String name, Class<?> targetType,
+                                   @Nullable Object defaultValue) {
     }
 
     /**
-     * Callback after {@link ConfigurablePropertyResolver#getProperty(String, String)}
-     *
-     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
-     * @param name             the name of the property
-     * @param value            the value of property
-     * @param defaultValue     the default value of the property
-     */
-    default void afterGetProperty(ConfigurablePropertyResolver propertyResolver, String name,
-                                  @Nullable String value, @Nullable String defaultValue) {
-    }
-
-    /**
-     * Callback before {@link ConfigurablePropertyResolver#getProperty(String, Class, Object)}
-     *
-     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
-     * @param name             the name of the property
-     * @param targetType       the target type
-     * @param defaultValue     the default value of the property
-     */
-    default void beforeGetProperty(ConfigurablePropertyResolver propertyResolver, String name,
-                                   Class<?> targetType, @Nullable Object defaultValue) {
-    }
-
-    /**
-     * Callback after {@link ConfigurablePropertyResolver#getProperty(String, Class, Object)}
-     *
-     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
-     * @param name             the name of the property
-     * @param targetType       the target type
-     * @param value            the value of property
-     * @param defaultValue     the default value of the property
-     */
-    default void afterGetProperty(ConfigurablePropertyResolver propertyResolver, String name,
-                                  Class<?> targetType, @Nullable Object value, @Nullable Object defaultValue) {
-    }
-
-    /**
-     * Callback before {@link ConfigurablePropertyResolver#getRequiredProperty(String, Class)}
-     *
-     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
-     * @param name             the name of the property
-     */
-    default void beforeGetRequiredProperty(ConfigurablePropertyResolver propertyResolver, String name) {
-    }
-
-    /**
-     * Callback after {@link ConfigurablePropertyResolver#getRequiredProperty(String, Class)}
+     * Callback after {@link PropertyResolver#getProperty(String, String)} or {@link PropertyResolver#getProperty(String, Class, Object)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param name             the name of the property
      * @param value            the value of property
+     * @param targetType       the target type to be converted by the property
+     * @param defaultValue     the default value of the target-typed property
      */
-    default void afterGetRequiredProperty(ConfigurablePropertyResolver propertyResolver, String name, Object value) {
+    default void afterGetProperty(ConfigurablePropertyResolver propertyResolver, String name, Class<?> targetType,
+                                  @Nullable Object value, @Nullable Object defaultValue) {
     }
 
     /**
-     * Callback before {@link ConfigurablePropertyResolver#resolvePlaceholders(String)}
+     * Callback before {@link PropertyResolver#getRequiredProperty(String)} or {@link PropertyResolver#getRequiredProperty(String, Class)}.
+     *
+     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
+     * @param name             the name of the property
+     * @param targetType       the target type to be converted by the property
+     */
+    default void beforeGetRequiredProperty(ConfigurablePropertyResolver propertyResolver, String name, Class<?> targetType) {
+    }
+
+    /**
+     * Callback after {@link PropertyResolver#getRequiredProperty(String)} or {@link PropertyResolver#getRequiredProperty(String, Class)}.
+     *
+     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
+     * @param name             the name of the property
+     * @param targetType       the target type to be converted by the property
+     * @param value            the value of the target-typed property
+     */
+    default void afterGetRequiredProperty(ConfigurablePropertyResolver propertyResolver, String name, Class<?> targetType, Object value) {
+    }
+
+    /**
+     * Callback before {@link PropertyResolver#resolvePlaceholders(String)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param text             the text to be resolved
@@ -121,17 +101,17 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback after {@link ConfigurablePropertyResolver#resolvePlaceholders(String)}
+     * Callback after {@link PropertyResolver#resolvePlaceholders(String)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
-     * @param text             the text to be resolved     *
+     * @param text             the text to be resolved
      * @param result           the resolved result
      */
     default void afterResolvePlaceholders(ConfigurablePropertyResolver propertyResolver, String text, String result) {
     }
 
     /**
-     * Callback before {@link ConfigurablePropertyResolver#resolveRequiredPlaceholders(String)}
+     * Callback before {@link ConfigurablePropertyResolver#resolveRequiredPlaceholders(String)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param text             the text to be resolved
@@ -140,7 +120,7 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback after {@link ConfigurablePropertyResolver#resolveRequiredPlaceholders(String)}
+     * Callback after {@link PropertyResolver#resolveRequiredPlaceholders(String)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param text             the text to be resolved
@@ -150,7 +130,7 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback before {@link ConfigurablePropertyResolver#setRequiredProperties(String...)}
+     * Callback before {@link ConfigurablePropertyResolver#setRequiredProperties(String...)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param properties       the required properties
@@ -159,7 +139,7 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback after {@link ConfigurablePropertyResolver#setRequiredProperties(String...)}
+     * Callback after {@link ConfigurablePropertyResolver#setRequiredProperties(String...)}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param properties       the required properties
@@ -168,7 +148,7 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback before {@link ConfigurablePropertyResolver#validateRequiredProperties()}
+     * Callback before {@link ConfigurablePropertyResolver#validateRequiredProperties()}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      */
@@ -176,7 +156,7 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback after {@link ConfigurablePropertyResolver#validateRequiredProperties()}
+     * Callback after {@link ConfigurablePropertyResolver#validateRequiredProperties()}.
      *
      * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      */
@@ -184,7 +164,24 @@ public interface PropertyResolverListener {
     }
 
     /**
-     * Callback before {@link ConfigurablePropertyResolver#setConversionService(ConfigurableConversionService)}
+     * Callback before {@link ConfigurablePropertyResolver#getConversionService()}.
+     *
+     * @param propertyResolver {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
+     */
+    default void beforeGetConversionService(ConfigurablePropertyResolver propertyResolver) {
+    }
+
+    /**
+     * Callback after {@link ConfigurablePropertyResolver#getConversionService()}.
+     *
+     * @param propertyResolver  {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
+     * @param conversionService {@link ConfigurableConversionService}
+     */
+    default void afterGetConversionService(ConfigurablePropertyResolver propertyResolver, ConfigurableConversionService conversionService) {
+    }
+
+    /**
+     * Callback before {@link ConfigurablePropertyResolver#setConversionService(ConfigurableConversionService)}.
      *
      * @param propertyResolver  {@link ConfigurablePropertyResolver the underlying ConfigurablePropertyResolver}
      * @param conversionService {@link ConfigurableConversionService}
