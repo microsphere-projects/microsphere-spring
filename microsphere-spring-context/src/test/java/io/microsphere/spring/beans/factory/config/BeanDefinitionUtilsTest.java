3882b6b1b9d11ae0011aa35fb277d7d74255637d
Mercy Ma <mercyblitz@gmail.com>
2024-12-27 09:51:55 +0800

    Update BeanDefinitionUtilsTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/config/BeanDefinitionUtilsTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/config/BeanDefinitionUtilsTest.java
@@ -207,7 +207,7 @@ public class BeanDefinitionUtilsTest {
     }
 
     private void testInSpringContainer(BiConsumer<ConfigurableApplicationContext, ConfigurableListableBeanFactory> contextConsumer) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(this.getClass());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanDefinitionUtils.class);
         contextConsumer.accept(context, context.getBeanFactory());
         context.close();
     }
