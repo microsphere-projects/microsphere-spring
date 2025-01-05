314dd23f12b8c823c54f21ad89cdbf510b69efb0
Mercy Ma <mercyblitz@gmail.com>
2025-01-05 17:47:31 +0800

    Update ListenableConfigurableEnvironment.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/ListenableConfigurableEnvironment.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/ListenableConfigurableEnvironment.java
@@ -31,7 +31,6 @@ import org.springframework.core.env.Profiles;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.invoke.MethodHandle;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
