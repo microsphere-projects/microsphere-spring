f84a355ca161e624727f927dec9be805f080819a
Mercy <mercyblitz@gmail.com>
2025-05-03 23:54:00 +0800

    Update BeanFactoryListenerAdapter.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/context/event/BeanFactoryListenerAdapter.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/context/event/BeanFactoryListenerAdapter.java
@@ -18,6 +18,7 @@ package io.microsphere.spring.context.event;
 
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 
 /**
  * The {@link BeanFactoryListener} Adapter
@@ -28,6 +29,8 @@ import org.springframework.beans.factory.support.BeanDefinitionRegistry;
  * @see EventPublishingBeanAfterProcessor
  * @see BeanFactoryListeners
  * @see BeanFactoryListener
+ * @see ConfigurableListableBeanFactory
+ * @see DefaultListableBeanFactory
  * @since 1.0.0
  */
 public interface BeanFactoryListenerAdapter extends BeanFactoryListener {
