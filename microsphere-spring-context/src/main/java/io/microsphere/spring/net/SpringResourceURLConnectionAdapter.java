498527744c90a87f3ae9ecfb4156986d299d9a82
Mercy <mercyblitz@gmail.com>
2025-05-05 19:33:49 +0800

    Update SpringResourceURLConnectionAdapter.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/net/SpringResourceURLConnectionAdapter.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/net/SpringResourceURLConnectionAdapter.java
@@ -17,24 +17,12 @@
 package io.microsphere.spring.net;
 
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.WritableResource;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import static io.microsphere.collection.ListUtils.first;
-import static java.util.Collections.emptyMap;
-import static java.util.Collections.unmodifiableMap;
 
 /**
  * The {@link URLConnection} adapter class is based on the Spring {@link Resource}
@@ -42,20 +30,10 @@ import static java.util.Collections.unmodifiableMap;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-class SpringResourceURLConnectionAdapter extends URLConnection {
+class SpringResourceURLConnectionAdapter extends AbstractSpringResourceURLConnection {
 
-    private final Resource resource;
-
-    private final WritableResource writableResource;
-
-    private MultiValueMap<String, String> requestProperties;
-
-    private MultiValueMap<String, String> headers;
-
-    protected SpringResourceURLConnectionAdapter(URL url, Resource resource) {
-        super(url);
-        this.resource = resource;
-        this.writableResource = resource instanceof WritableResource ? (WritableResource) resource : null;
+    SpringResourceURLConnectionAdapter(URL url, Resource resource) {
+        super(url, resource);
     }
 
     @Override
@@ -84,52 +62,6 @@ class SpringResourceURLConnectionAdapter extends URLConnection {
     }
 
     @Override
-    public Object getContent() throws IOException {
-        return super.getContent();
-    }
-
-    @Override
-    public Object getContent(Class[] classes) throws IOException {
-        return super.getContent(classes);
-    }
-
-    @Override
-    public void setDoOutput(boolean dooutput) {
-        if (writableResource == null && dooutput) {
-            throw new UnsupportedOperationException("The resource does not support output!");
-        }
-        super.setDoOutput(dooutput);
-    }
-
-    @Override
-    public String getHeaderField(String name) {
-        MultiValueMap<String, String> headers = getHeaders();
-        return headers.getFirst(name);
-    }
-
-    @Override
-    public Map<String, List<String>> getHeaderFields() {
-        MultiValueMap<String, String> headers = getHeaders();
-        return headers.isEmpty() ? emptyMap() : unmodifiableMap(headers);
-    }
-
-    @Override
-    public String getHeaderFieldKey(int n) {
-        Map.Entry<String, List<String>> entry = getHeaderEntry(n);
-        return entry == null ? null : entry.getKey();
-    }
-
-    @Override
-    public String getHeaderField(int n) {
-        Map.Entry<String, List<String>> entry = getHeaderEntry(n);
-        if (entry == null) {
-            return null;
-        }
-        List<String> value = entry.getValue();
-        return first(value);
-    }
-
-    @Override
     public InputStream getInputStream() throws IOException {
         return resource.getInputStream();
     }
@@ -142,87 +74,4 @@ class SpringResourceURLConnectionAdapter extends URLConnection {
         return writableResource.getOutputStream();
     }
 
-    @Override
-    public void setRequestProperty(String key, String value) {
-        MultiValueMap<String, String> requestProperties = doGetRequestProperties();
-        List<String> values = new ArrayList<>(1);
-        values.add(value);
-        requestProperties.put(key, values);
-    }
-
-    @Override
-    public void addRequestProperty(String key, String value) {
-        MultiValueMap<String, String> requestProperties = doGetRequestProperties();
-        requestProperties.add(key, value);
-    }
-
-    @Override
-    public String getRequestProperty(String key) {
-        MultiValueMap<String, String> requestProperties = doGetRequestProperties();
-        return requestProperties.getFirst(key);
-    }
-
-    @Override
-    public Map<String, List<String>> getRequestProperties() {
-        return unmodifiableMap(doGetRequestProperties());
-    }
-
-    @Override
-    public void connect() throws IOException {
-        super.connected = true;
-    }
-
-    public void disconnect() throws IOException {
-        super.connected = false;
-    }
-
-    public boolean isConnected() {
-        return super.connected;
-    }
-
-    @Override
-    public String toString() {
-        return "SpringResourceURLConnectionAdapter{" +
-                "url=" + url +
-                ", resource=" + resource +
-                '}';
-    }
-
-    protected Map.Entry<String, List<String>> getHeaderEntry(int n) {
-        MultiValueMap<String, String> headers = getHeaders();
-        if (n < 0 || n > headers.size() - 1) { // out of the index range
-            return null;
-        }
-        Set<Map.Entry<String, List<String>>> entries = headers.entrySet();
-        Iterator<Map.Entry<String, List<String>>> iterator = entries.iterator();
-        Map.Entry<String, List<String>> entry = null;
-        for (int i = 0; i <= n & iterator.hasNext(); i++) {
-            entry = iterator.next();
-        }
-        return entry;
-    }
-
-    protected MultiValueMap<String, String> getHeaders() {
-        MultiValueMap<String, String> headers = this.headers;
-        if (headers == null) {
-            headers = new LinkedMultiValueMap<>();
-            this.headers = headers;
-        }
-        return headers;
-    }
-
-    protected SpringResourceURLConnectionAdapter addHeader(String name, String value) {
-        MultiValueMap<String, String> headers = getHeaders();
-        headers.add(name, value);
-        return this;
-    }
-
-    protected MultiValueMap<String, String> doGetRequestProperties() {
-        MultiValueMap<String, String> requestProperties = this.requestProperties;
-        if (requestProperties == null) {
-            requestProperties = new LinkedMultiValueMap<>();
-            this.requestProperties = requestProperties;
-        }
-        return requestProperties;
-    }
 }
