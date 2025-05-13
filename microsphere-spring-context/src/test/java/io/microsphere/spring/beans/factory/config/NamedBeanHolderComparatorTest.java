7d6fc56e52679e2f53783f6ef3ada1922a483ef0
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 22:49:05 +0800

    Create NamedBeanHolderComparatorTest.java

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/config/NamedBeanHolderComparatorTest.java
@@ -0,0 +1,27 @@
+package io.microsphere.spring.beans.factory.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.NamedBeanHolder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link NamedBeanHolderComparator} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see NamedBeanHolderComparator
+ * @since 1.0.0
+ */
+public class NamedBeanHolderComparatorTest {
+
+    @Test
+    public void testCompare() {
+        NamedBeanHolder namedBeanHolder1 = createNamedBeanHolder("bean1");
+        NamedBeanHolder namedBeanHolder2 = createNamedBeanHolder("bean2");
+        assertEquals(0, NamedBeanHolderComparator.INSTANCE.compare(namedBeanHolder1, namedBeanHolder2));
+    }
+
+    private NamedBeanHolder createNamedBeanHolder(String name) {
+        return new NamedBeanHolder(name, name);
+    }
+}
