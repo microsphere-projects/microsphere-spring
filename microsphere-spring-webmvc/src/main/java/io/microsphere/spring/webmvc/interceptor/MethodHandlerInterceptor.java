171a8f7c11b6218f4d573cb95e399836cf2afd02
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 17:18:33 +0800

    Update MethodHandlerInterceptor.java

--- microsphere-spring-webmvc/src/main/java/io/microsphere/spring/webmvc/interceptor/MethodHandlerInterceptor.java
+++ microsphere-spring-webmvc/src/main/java/io/microsphere/spring/webmvc/interceptor/MethodHandlerInterceptor.java
@@ -9,7 +9,7 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Nullable;
+import io.microsphere.annotation.Nullable;
 
 /**
  *
