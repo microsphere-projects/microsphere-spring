cc91ff5dbd8b3ad3b0a3674ef66338b8cfb4a1e3
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 22:49:39 +0800

    Create ImmutableMapPropertySourceTest.java

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/config/env/ImmutableMapPropertySourceTest.java
@@ -0,0 +1,60 @@
+package io.microsphere.spring.config.env;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.util.Collections.emptyMap;
+import static java.util.Collections.unmodifiableMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link ImmutableMapPropertySource} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ImmutableMapPropertySource
+ * @since 1.0.0
+ */
+public class ImmutableMapPropertySourceTest {
+
+    private static final String NAME = "test";
+
+    private static final Class<? extends Map> SOURCE_CLASS = unmodifiableMap(emptyMap()).getClass();
+
+    @Test
+    public void testNewFromSortedMap() {
+        ImmutableMapPropertySource propertySource = createPropertySource(new TreeMap<>());
+        assertPropertySource(propertySource);
+    }
+
+    @Test
+    public void testNewFromLinkedHashMap() {
+        ImmutableMapPropertySource propertySource = createPropertySource(new LinkedHashMap<>());
+        assertPropertySource(propertySource);
+    }
+
+    @Test
+    public void testNewFromIdentityHashMap() {
+        ImmutableMapPropertySource propertySource = createPropertySource(new IdentityHashMap<>());
+        assertPropertySource(propertySource);
+    }
+
+    @Test
+    public void testNewFromHashMap() {
+        ImmutableMapPropertySource propertySource = createPropertySource(new HashMap<>());
+        assertPropertySource(propertySource);
+    }
+
+    void assertPropertySource(ImmutableMapPropertySource propertySource) {
+        assertEquals(NAME, propertySource.getName());
+        assertEquals(SOURCE_CLASS, propertySource.getSource().getClass());
+    }
+
+    private ImmutableMapPropertySource createPropertySource(Map source) {
+        return new ImmutableMapPropertySource(NAME, source);
+    }
+}
