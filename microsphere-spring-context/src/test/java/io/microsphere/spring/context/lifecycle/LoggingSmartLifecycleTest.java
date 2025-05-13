729d93adfda0989ca351d8595af9b4dab5a38f52
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 22:50:09 +0800

    Create LoggingSmartLifecycleTest.java

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/lifecycle/LoggingSmartLifecycleTest.java
@@ -0,0 +1,108 @@
+package io.microsphere.spring.context.lifecycle;
+
+import io.microsphere.logging.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.microsphere.logging.LoggerFactory.getLogger;
+import static io.microsphere.spring.context.lifecycle.AbstractSmartLifecycle.DEFAULT_PHASE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * {@link LoggingSmartLifecycle} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see LoggingSmartLifecycle
+ * @since 1.0.0
+ */
+public class LoggingSmartLifecycleTest {
+
+    private static final Logger logger = getLogger(LoggingSmartLifecycleTest.class);
+
+    private LoggingSmartLifecycle lifecycle;
+
+    @BeforeEach
+    public void before() {
+        lifecycle = new LoggingSmartLifecycle();
+    }
+
+    @Test
+    public void testIsAutoStartup() {
+        assertTrue(lifecycle.isAutoStartup());
+    }
+
+    @Test
+    public void testStop() {
+        lifecycle.stop();
+        assertFalse(lifecycle.isStarted());
+    }
+
+    @Test
+    public void testGetPhase() {
+        assertEquals(DEFAULT_PHASE, lifecycle.getPhase());
+    }
+
+    @Test
+    public void testStart() {
+        assertFalse(lifecycle.isStarted());
+        lifecycle.start();
+        assertTrue(lifecycle.isStarted());
+    }
+
+    @Test
+    public void testDoStart() {
+        assertFalse(lifecycle.isStarted());
+        lifecycle.doStart();
+        assertFalse(lifecycle.isStarted());
+    }
+
+    @Test
+    public void testStopWithRunnable() {
+        assertFalse(lifecycle.isStarted());
+        lifecycle.stop(() -> {
+            logger.trace("stop running");
+        });
+        assertFalse(lifecycle.isStarted());
+    }
+
+    @Test
+    public void testDoStop() {
+        assertFalse(lifecycle.isStarted());
+        lifecycle.doStop();
+        assertFalse(lifecycle.isStarted());
+    }
+
+    @Test
+    public void testIsRunning() {
+        assertFalse(lifecycle.isRunning());
+        lifecycle.start();
+        assertTrue(lifecycle.isRunning());
+    }
+
+    @Test
+    public void testIsStarted() {
+        assertFalse(lifecycle.isStarted());
+
+        lifecycle.start();
+        assertTrue(lifecycle.isStarted());
+
+        lifecycle.stop();
+        assertFalse(lifecycle.isStarted());
+    }
+
+    @Test
+    public void testSetPhase() {
+        int phase = 1;
+        lifecycle.setPhase(phase);
+        assertEquals(phase, lifecycle.getPhase());
+    }
+
+    @Test
+    public void testSetStarted() {
+        assertFalse(lifecycle.isStarted());
+        lifecycle.setStarted(true);
+        assertTrue(lifecycle.isStarted());
+    }
+}
