23e2ead92ea60101604bbde20863e801e98ff41a
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 22:49:16 +0800

    Update ListenableAutowireCandidateResolverTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/support/ListenableAutowireCandidateResolverTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/support/ListenableAutowireCandidateResolverTest.java
@@ -6,17 +6,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.DependencyDescriptor;
+import org.springframework.beans.factory.support.AutowireCandidateResolver;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.mock.env.MockPropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.lang.reflect.Field;
+
+import static io.microsphere.reflect.FieldUtils.findField;
+import static io.microsphere.spring.beans.factory.support.ListenableAutowireCandidateResolver.ENABLED_PROPERTY_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * {@link ListenableAutowireCandidateResolver} Test
@@ -46,13 +59,17 @@ public class ListenableAutowireCandidateResolverTest implements AutowireCandidat
     private String testName;
 
     @Autowired
+    @Qualifier("testBean")
     @Lazy
     private TestBean testBean;
 
     @Autowired
     private ObjectProvider<ListenableAutowireCandidateResolver> resolverProvider;
 
-    private Environment environment;
+    private ConfigurableEnvironment environment;
+
+    @Autowired
+    private DefaultListableBeanFactory beanFactory;
 
     private static Object resolvedTestName;
 
@@ -63,6 +80,76 @@ public class ListenableAutowireCandidateResolverTest implements AutowireCandidat
 
         ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
         assertNotNull(resolver.cloneIfNecessary());
+    }
+
+    @Test
+    public void testAddListeners() {
+        ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
+        resolver.addListener(new LoggingAutowireCandidateResolvingListener());
+    }
+
+    @Test
+    public void testIsRequired() {
+        testIsRequired(true);
+        testIsRequired(false);
+    }
+
+    void testIsRequired(boolean required) {
+        ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
+        Field field = findField(this.getClass(), "testName");
+        DependencyDescriptor dependencyDescriptor = new DependencyDescriptor(field, required);
+        assertEquals(required, resolver.isRequired(dependencyDescriptor));
+    }
+
+    @Test
+    public void testHasQualifier() {
+        ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
+        Field field = findField(this.getClass(), "testBean");
+        DependencyDescriptor dependencyDescriptor = new DependencyDescriptor(field, true);
+        assertEquals(true, resolver.hasQualifier(dependencyDescriptor));
+
+        field = findField(this.getClass(), "testName");
+        dependencyDescriptor = new DependencyDescriptor(field, true);
+        assertFalse(resolver.hasQualifier(dependencyDescriptor));
+    }
+
+    @Test
+    public void testHasQualifierOnNull() {
+        ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
+        assertThrows(NullPointerException.class, () -> assertFalse(resolver.hasQualifier(null)));
+    }
+
+    @Test
+    public void testWrap() {
+        // wrap(BeanFactory) method was invoked on BeanFactoryPostProcessor lifecycle
+        ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
+        AutowireCandidateResolver autowireCandidateResolver = beanFactory.getAutowireCandidateResolver();
+        assertSame(autowireCandidateResolver, resolver);
+
+        // rewrap , however it's not necessary
+        resolver.wrap(beanFactory);
+        autowireCandidateResolver = beanFactory.getAutowireCandidateResolver();
+        assertSame(autowireCandidateResolver, resolver);
+    }
+
+    @Test
+    public void testWrapOnDisabled() {
+        MutablePropertySources mutablePropertySources = this.environment.getPropertySources();
+        MockPropertySource mockPropertySource = new MockPropertySource();
+        mutablePropertySources.addFirst(mockPropertySource);
+
+        // wrap(BeanFactory) method was invoked on BeanFactoryPostProcessor lifecycle
+        ListenableAutowireCandidateResolver resolver = resolverProvider.getIfAvailable();
+        AutowireCandidateResolver autowireCandidateResolver = beanFactory.getAutowireCandidateResolver();
+        assertSame(autowireCandidateResolver, resolver);
+
+        // set "microsphere.spring.listenable-autowire-candidate-resolver.enabled" to be "false"
+        mockPropertySource.withProperty(ENABLED_PROPERTY_NAME, "false");
+
+        // rewrap , obviously it does not work
+        resolver.wrap(beanFactory);
+        autowireCandidateResolver = beanFactory.getAutowireCandidateResolver();
+        assertSame(autowireCandidateResolver, resolver);
     }
 
     @Override
@@ -76,6 +163,6 @@ public class ListenableAutowireCandidateResolverTest implements AutowireCandidat
 
     @Override
     public void setEnvironment(Environment environment) {
-        this.environment = environment;
+        this.environment = (ConfigurableEnvironment) environment;
     }
 }
