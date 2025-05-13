ae4b078f56461b8ef6a42902c8fdc9c45dbf8c0a
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 22:48:29 +0800

    Update PropertySourcesUtils.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/PropertySourcesUtils.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/PropertySourcesUtils.java
@@ -2,7 +2,7 @@ package io.microsphere.spring.core.env;
 
 import io.microsphere.annotation.Nonnull;
 import io.microsphere.logging.Logger;
-import io.microsphere.util.BaseUtils;
+import io.microsphere.util.Utils;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 import org.springframework.core.env.Environment;
@@ -38,7 +38,7 @@ import static java.util.Collections.unmodifiableSet;
  * @see PropertySources
  * @since 1.0.0
  */
-public abstract class PropertySourcesUtils extends BaseUtils {
+public abstract class PropertySourcesUtils implements Utils {
 
     private static final Logger logger = getLogger(PropertySourcesUtils.class);
 
@@ -384,6 +384,9 @@ public abstract class PropertySourcesUtils extends BaseUtils {
      */
     public static boolean containsBootstrapPropertySource(ConfigurableEnvironment environment) {
         return containsPropertySource(environment, BOOTSTRAP_PROPERTY_SOURCE_NAME);
+    }
+
+    private PropertySourcesUtils() {
     }
 }
 
