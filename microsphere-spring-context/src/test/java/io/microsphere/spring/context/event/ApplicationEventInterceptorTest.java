ddd790ff22bda57f540c31f4a0d6b403eed54ce5
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 22:49:45 +0800

    Create ApplicationEventInterceptorTest.java

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/event/ApplicationEventInterceptorTest.java
@@ -0,0 +1,42 @@
+package io.microsphere.spring.context.event;
+
+import io.microsphere.logging.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.microsphere.logging.LoggerFactory.getLogger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
+
+/**
+ * {@link ApplicationEventInterceptor} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ApplicationEventInterceptor
+ * @since 1.0.0
+ */
+public class ApplicationEventInterceptorTest {
+
+    private static final Logger logger = getLogger(ApplicationEventInterceptorTest.class);
+
+    private ApplicationEventInterceptor interceptor;
+
+    @BeforeEach
+    public void before() {
+        this.interceptor = (event, eventType, chain) -> {
+            if (logger.isTraceEnabled()) {
+                logger.trace("event : {} , eventType : {} , chain : {}", event, eventType, chain);
+            }
+        };
+    }
+
+    @Test
+    public void testIntercept() {
+        this.interceptor.intercept(null, null, null);
+    }
+
+    @Test
+    public void testGetOrder() {
+        assertEquals(LOWEST_PRECEDENCE, this.interceptor.getOrder());
+    }
+}
