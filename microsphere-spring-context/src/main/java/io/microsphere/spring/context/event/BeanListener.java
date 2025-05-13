faf41d100acc1c1fff3de9bbcc852d2682d7007e
Mercy Ma <mercyblitz@gmail.com>
2025-05-13 21:49:27 +0800

    Update BeanListener.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/context/event/BeanListener.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/context/event/BeanListener.java
@@ -27,6 +27,11 @@ import java.util.EventListener;
  * The {@link EventListener} for Bean lifecycle
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see EventPublishingBeanInitializer
+ * @see EventPublishingBeanBeforeProcessor
+ * @see EventPublishingBeanAfterProcessor
+ * @see BeanListeners
+ * @see BeanListenerAdapter
  * @since 1.0.0
  */
 public interface BeanListener extends EventListener {
