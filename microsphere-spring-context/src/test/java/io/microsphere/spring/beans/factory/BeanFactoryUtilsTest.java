8ed57a915e148ffab5539dda5f467a58aae92523
Mercy Ma <mercyblitz@gmail.com>
2025-05-15 10:00:30 +0800

    Update BeanFactoryUtilsTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/BeanFactoryUtilsTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/BeanFactoryUtilsTest.java
@@ -185,77 +185,63 @@ public class BeanFactoryUtilsTest {
     @Test
     public void testGetBeanPostProcessorsOnNotAbstractFactory() {
         List<BeanPostProcessor> beanPostProcessors = getBeanPostProcessors(new BeanFactory() {
-            @Override
+
             public Object getBean(String name) throws BeansException {
                 return null;
             }
 
-            @Override
             public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
                 return null;
             }
 
-            @Override
             public Object getBean(String name, Object... args) throws BeansException {
                 return null;
             }
 
-            @Override
             public <T> T getBean(Class<T> requiredType) throws BeansException {
                 return null;
             }
 
-            @Override
             public <T> T getBean(Class<T> requiredType, Object... args) throws BeansException {
                 return null;
             }
 
-            @Override
             public <T> ObjectProvider<T> getBeanProvider(Class<T> requiredType) {
                 return null;
             }
 
-            @Override
             public <T> ObjectProvider<T> getBeanProvider(ResolvableType requiredType) {
                 return null;
             }
 
-            @Override
             public boolean containsBean(String name) {
                 return false;
             }
 
-            @Override
             public boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
                 return false;
             }
 
-            @Override
             public boolean isPrototype(String name) throws NoSuchBeanDefinitionException {
                 return false;
             }
 
-            @Override
             public boolean isTypeMatch(String name, ResolvableType typeToMatch) throws NoSuchBeanDefinitionException {
                 return false;
             }
 
-            @Override
             public boolean isTypeMatch(String name, Class<?> typeToMatch) throws NoSuchBeanDefinitionException {
                 return false;
             }
 
-            @Override
             public Class<?> getType(String name) throws NoSuchBeanDefinitionException {
                 return null;
             }
 
-            @Override
             public Class<?> getType(String name, boolean allowFactoryBeanInit) throws NoSuchBeanDefinitionException {
                 return null;
             }
 
-            @Override
             public String[] getAliases(String name) {
                 return new String[0];
             }
