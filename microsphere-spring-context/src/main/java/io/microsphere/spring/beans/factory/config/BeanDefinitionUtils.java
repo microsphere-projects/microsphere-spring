2447977874e0eeebb0aee5971e4042be19d5dbeb
Mercy <mercyblitz@gmail.com>
2025-07-14 15:03:39 +0800

    Update BeanDefinitionUtils.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/beans/factory/config/BeanDefinitionUtils.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/beans/factory/config/BeanDefinitionUtils.java
@@ -39,7 +39,6 @@ import static io.microsphere.lang.function.Predicates.and;
 import static io.microsphere.logging.LoggerFactory.getLogger;
 import static io.microsphere.spring.util.MethodHandleUtils.handleInvokeExactFailure;
 import static io.microsphere.util.ArrayUtils.EMPTY_OBJECT_ARRAY;
-import static io.microsphere.util.ArrayUtils.arrayToString;
 import static io.microsphere.util.ArrayUtils.length;
 import static io.microsphere.util.ClassLoaderUtils.getDefaultClassLoader;
 import static io.microsphere.util.ClassLoaderUtils.resolveClass;
