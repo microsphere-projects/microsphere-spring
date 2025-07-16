b19fde1c31110f95f986fc7f198549633939300a
Mercy <mercyblitz@gmail.com>
2025-07-16 17:31:32 +0800

    Create JoinAliasGenerator.java

---
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/beans/factory/support/JoinAliasGenerator.java
@@ -0,0 +1,33 @@
+package io.microsphere.spring.beans.factory.support;
+
+import static io.microsphere.util.StringUtils.split;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+import static org.springframework.util.StringUtils.capitalize;
+
+/**
+ * A {@link ConfigurationBeanAliasGenerator} implementation that generates aliases by joining
+ * the parts of the prefix with the bean name in a cohesive format.
+ *
+ * <p>
+ * For example, given a prefix like "user.config" and a bean name "dataSource", it would generate an alias like:
+ * {@code userConfig + jointSymbol() + dataSource}, where the joint symbol is defined by the implementing subclass.
+ * </p>
+ *
+ * @author <a href="mailto:15868175516@163.com">qi.li</a>
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since 1.0.0
+ */
+public abstract class JoinAliasGenerator implements ConfigurationBeanAliasGenerator {
+
+    public String generateAlias(String prefix, String beanName, Class<?> configClass) {
+        String[] prefixArray = split(prefix, ".");
+        String first = prefixArray[0];
+        String others = stream(prefixArray).skip(1)
+                .map(item -> capitalize(item))
+                .collect(joining());
+        return first + others + separator() + beanName;
+    }
+
+    protected abstract String separator();
+}
