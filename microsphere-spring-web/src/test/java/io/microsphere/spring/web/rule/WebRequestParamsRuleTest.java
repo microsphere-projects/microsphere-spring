94d2e7c455888cbcbbff62a96e2356700103402a
Mercy <mercyblitz@gmail.com>
2025-07-19 20:29:16 +0800

    Create WebRequestParamsRuleTest.java

---
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestParamsRuleTest.java
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.web.rule;
+
+import io.microsphere.collection.MapUtils;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * {@link WebRequestParamsRule} Test
+ */
+public class WebRequestParamsRuleTest {
+
+    private NativeWebRequest createWebRequest(Object... values) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        Map<String, Object> params = MapUtils.of(values);
+        request.setParameters(params);
+        return new ServletWebRequest(request);
+    }
+
+    // ==================== Constructor ====================
+    @Test
+    public void testConstructorWithParams() {
+        WebRequestParamsRule rule = new WebRequestParamsRule("param1=value1", "param2=value2");
+        assertEquals(2, rule.getContent().size());
+    }
+
+    @Test
+    public void testConstructorWithEmptyParams() {
+        WebRequestParamsRule rule = new WebRequestParamsRule();
+        assertTrue(rule.getContent().isEmpty());
+    }
+
+    // ==================== getContent() ====================
+    @Test
+    public void testGetContent() {
+        WebRequestParamsRule rule = new WebRequestParamsRule("param1", "param2=value2");
+        assertEquals("Should return correct number of expressions", 2, rule.getContent().size());
+    }
+
+    // ==================== getToStringInfix() ====================
+    @Test
+    public void testGetToStringInfix() {
+        WebRequestParamsRule rule = new WebRequestParamsRule();
+        assertEquals(" && ", rule.getToStringInfix());
+    }
+
+    // ==================== matches() ====================
+    @Test
+    public void testMatchesAllConditions() {
+        WebRequestParamsRule rule = new WebRequestParamsRule("param1=value1", "param2=value2");
+        NativeWebRequest request = createWebRequest("param1", "value1", "param2", new String[]{"value2"});
+        assertTrue(rule.matches(request));
+    }
+
+    @Test
+    public void testMatchesWithOneMismatch() {
+        WebRequestParamsRule rule = new WebRequestParamsRule("param1=value1", "param2=value2");
+        NativeWebRequest request = createWebRequest("param1", "wrongValue");
+        assertFalse(rule.matches(request));
+    }
+
+    @Test
+    public void testMatchesWithEmptyExpressions() {
+        WebRequestParamsRule rule = new WebRequestParamsRule();
+        NativeWebRequest request = createWebRequest("anyParam", "anyValue");
+        assertTrue("Should always match when no expressions", rule.matches(request));
+    }
+
+    @Test
+    public void testMatchesWithMissingParameter() {
+        WebRequestParamsRule rule = new WebRequestParamsRule("requiredParam=value");
+        NativeWebRequest request = createWebRequest("otherParam", "value");
+        assertFalse(rule.matches(request));
+    }
+
+    @Test
+    public void testMatchesWithNegatedExpression() {
+        WebRequestParamsRule rule = new WebRequestParamsRule("param1!=value1");
+        NativeWebRequest request = createWebRequest("param1", "value1");
+        assertFalse(rule.matches(request));
+    }
+
+    @Test
+    public void testMatchesWithComplexExpressions() {
+        WebRequestParamsRule rule = new WebRequestParamsRule(
+                "param1=value1",
+                "param2",
+                "!param3"
+        );
+
+        NativeWebRequest request = createWebRequest("param1", "value1", "param2", new String[]{"anyValue"});
+        assertTrue(rule.matches(request));
+    }
+}
