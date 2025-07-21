79a551af0887f80e459403395958fa1100afdd96
Mercy <mercyblitz@gmail.com>
2025-07-21 23:06:00 +0800

    Create AbstractWebEndpointMappingFactoryTest.java

---
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/metadata/AbstractWebEndpointMappingFactoryTest.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.web.metadata;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static io.microsphere.spring.web.metadata.WebEndpointMapping.of;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * {@link AbstractWebEndpointMappingFactory} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see AbstractWebEndpointMappingFactory
+ * @since 1.0.0
+ */
+public class AbstractWebEndpointMappingFactoryTest {
+
+    static class TestWebEndpointMappingFactory extends AbstractWebEndpointMappingFactory<String> {
+
+        private WebEndpointMapping<?> webEndpointMapping;
+
+        TestWebEndpointMappingFactory(WebEndpointMapping<?> webEndpointMapping) {
+            this.webEndpointMapping = webEndpointMapping;
+        }
+
+        @Override
+        protected WebEndpointMapping<?> doCreate(String endpoint) throws Throwable {
+            if (this.webEndpointMapping == null) {
+                throw new Throwable("webEndpointMapping == null");
+            }
+            return this.webEndpointMapping;
+        }
+    }
+
+    private AbstractWebEndpointMappingFactory<String> factory;
+
+    @Before
+    public void setUp() throws Exception {
+        this.factory = new TestWebEndpointMappingFactory(of("/test").build());
+    }
+
+    @Test
+    public void testSupports() {
+        assertTrue(this.factory.supports(null));
+    }
+
+    @Test
+    public void testCreate() {
+        Optional<WebEndpointMapping<String>> webEndpointMapping = factory.create(null);
+        assertTrue(webEndpointMapping.isPresent());
+        webEndpointMapping.ifPresent(mapping -> assertEquals(of("/test").build(), mapping));
+    }
+
+    @Test
+    public void testCreateOnFailed() {
+        this.factory = new TestWebEndpointMappingFactory(null);
+        Optional<WebEndpointMapping<String>> webEndpointMapping = factory.create(null);
+        assertFalse(webEndpointMapping.isPresent());
+    }
+
+    @Test
+    public void testGetSourceType() {
+        assertEquals(String.class, this.factory.getSourceType());
+    }
+
+}
