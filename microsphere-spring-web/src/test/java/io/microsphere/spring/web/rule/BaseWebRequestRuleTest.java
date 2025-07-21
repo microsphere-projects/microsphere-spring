ac86eb948366abc23d35c50b28c95f072f3f9eaf
Mercy <mercyblitz@gmail.com>
2025-07-21 20:23:51 +0800

    Update BaseWebRequestRuleTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/BaseWebRequestRuleTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/BaseWebRequestRuleTest.java
@@ -17,19 +17,8 @@
 
 package io.microsphere.spring.web.rule;
 
+import io.microsphere.spring.web.BaseWebTest;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.ServletWebRequest;
-
-import java.util.Map;
-import java.util.function.Consumer;
-
-import static io.microsphere.collection.MapUtils.of;
-import static io.microsphere.spring.web.util.WebRequestUtils.PATH_ATTRIBUTE;
-import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD;
-import static org.springframework.http.HttpHeaders.ORIGIN;
-import static org.springframework.http.HttpMethod.OPTIONS;
 
 /**
  * Base {@link WebRequestRule} Test
@@ -38,50 +27,7 @@ import static org.springframework.http.HttpMethod.OPTIONS;
  * @see WebRequestRule
  * @since 1.0.0
  */
-public abstract class BaseWebRequestRuleTest {
-    protected NativeWebRequest createWebRequest() {
-        return createWebRequest(r -> {
-        });
-    }
-
-    protected NativeWebRequest createWebRequest(Consumer<MockHttpServletRequest> requestBuilder) {
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        requestBuilder.accept(request);
-        return new ServletWebRequest(request);
-    }
-
-    protected NativeWebRequest createWebRequest(String requestURI) {
-        return createWebRequest(request -> {
-            request.setRequestURI(requestURI);
-            request.setAttribute(PATH_ATTRIBUTE, requestURI);
-        });
-    }
-
-    protected NativeWebRequest createWebRequestWithParams(Object... params) {
-        return createWebRequest(request -> {
-            request.setParameters(of(params));
-        });
-    }
-
-    protected NativeWebRequest createWebRequestWithHeaders(Object... headers) {
-        return createWebRequestWithHeaders(of(headers));
-    }
-
-    protected NativeWebRequest createWebRequestWithHeaders(Map<String, String> headers) {
-        return createWebRequest(request -> {
-            headers.forEach(request::addHeader);
-        });
-    }
-
-    protected NativeWebRequest createPreFightRequest() {
-        // Create pre-flight request (OPTIONS method with Origin header)
-        return createWebRequest(request -> {
-            request.setMethod(OPTIONS.name());
-            request.addHeader(":METHOD:", request.getMethod());
-            request.addHeader(ORIGIN, "http://example.com");
-            request.addHeader(ACCESS_CONTROL_REQUEST_METHOD, "POST");
-        });
-    }
+public abstract class BaseWebRequestRuleTest extends BaseWebTest {
 
     @Test
     public void testGetToStringInfix() {
