52c43db639a3d0a01c95034ac1341f28c815b748
Mercy <mercyblitz@gmail.com>
2025-07-23 10:58:09 +0800

    Update EnableEmbeddedDatabases.java

--- microsphere-spring-test/src/main/java/io/microsphere/spring/test/jdbc/embedded/EnableEmbeddedDatabases.java
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/jdbc/embedded/EnableEmbeddedDatabases.java
@@ -20,12 +20,14 @@ package io.microsphere.spring.test.jdbc.embedded;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * {@link Repeatable} {@link EnableEmbeddedDatabase}
@@ -33,8 +35,8 @@ import java.lang.annotation.Target;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @since 1.0.0
  */
-@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
-@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
 @Documented
 @Inherited
 @Import(EmbeddedDataBaseBeanDefinitionsRegistrar.class)
