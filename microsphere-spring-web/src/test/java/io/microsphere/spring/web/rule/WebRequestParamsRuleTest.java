1d5cbfe1979e1fc468da18b5c3e1f2e85fdd18bd
Mercy <mercyblitz@gmail.com>
2025-07-24 19:44:04 +0800

    Update WebRequestParamsRuleTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestParamsRuleTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestParamsRuleTest.java
@@ -20,6 +20,7 @@ package io.microsphere.spring.web.rule;
 import org.junit.Test;
 import org.springframework.web.context.request.NativeWebRequest;
 
+import static io.microsphere.spring.test.util.SpringTestWebUtils.createWebRequestWithParams;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
