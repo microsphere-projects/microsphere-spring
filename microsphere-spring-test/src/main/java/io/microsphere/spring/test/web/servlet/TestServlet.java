c19481ee8bb39c981486adb5c2eff509fccc2725
Mercy <mercyblitz@gmail.com>
2025-07-24 13:23:23 +0800

    Override service method in TestServlet
    Added an explicit override of the service method in TestServlet to allow for future customization and extension. Currently, it delegates to the superclass implementation.

--- microsphere-spring-test/src/main/java/io/microsphere/spring/test/web/servlet/TestServlet.java
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/web/servlet/TestServlet.java
@@ -17,7 +17,11 @@
 package io.microsphere.spring.test.web.servlet;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * {@link Servlet} for Testing
@@ -26,4 +30,9 @@ import javax.servlet.http.HttpServlet;
  * @since 1.0.0
  */
 public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
 }
