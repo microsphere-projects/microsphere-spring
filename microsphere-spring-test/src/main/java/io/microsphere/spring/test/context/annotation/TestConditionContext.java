1b233d0e8e9a757f2fb238fb727acbf4cb9b29ed
Mercy <mercyblitz@gmail.com>
2025-07-24 13:30:49 +0800

    Create TestConditionContext.java

---
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/context/annotation/TestConditionContext.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.test.context.annotation;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+
+/**
+ * {@link ConditionContext} test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @since 1.0.0
+ */
+public class TestConditionContext implements ConditionContext, ApplicationContextAware {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public BeanDefinitionRegistry getRegistry() {
+        return (BeanDefinitionRegistry) applicationContext.getBeanFactory();
+    }
+
+    @Override
+    public ConfigurableListableBeanFactory getBeanFactory() {
+        return applicationContext.getBeanFactory();
+    }
+
+    @Override
+    public Environment getEnvironment() {
+        return applicationContext.getEnvironment();
+    }
+
+    @Override
+    public ResourceLoader getResourceLoader() {
+        return applicationContext;
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return applicationContext.getClassLoader();
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = (ConfigurableApplicationContext) applicationContext;
+    }
+}
