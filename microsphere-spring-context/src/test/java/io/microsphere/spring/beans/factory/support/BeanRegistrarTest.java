a06c74206d65ba867fe21cf083912f300a3526cd
Mercy <mercyblitz@gmail.com>
2025-07-24 20:30:45 +0800

    Update BeanRegistrarTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/support/BeanRegistrarTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/support/BeanRegistrarTest.java
@@ -19,7 +19,7 @@ package io.microsphere.spring.beans.factory.support;
 import io.microsphere.spring.test.Bean;
 import io.microsphere.spring.test.TestBean;
 import io.microsphere.spring.test.TestBean2;
-import io.microsphere.spring.util.User;
+import io.microsphere.spring.test.domain.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
