fc63a61bdf2414c425e9b275c69dd2104191be10
Mercy <mercyblitz@gmail.com>
2025-07-24 19:21:37 +0800

    Create TestRestController.java

---
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/web/controller/TestRestController.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.test.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Test {@link RestController @RestController}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see Controller
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestRestController {
+
+    @GetMapping("/helloworld")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/greeting/{message}")
+    public String greeting(@PathVariable String message) {
+        return "Greeting : " + message;
+    }
+}
