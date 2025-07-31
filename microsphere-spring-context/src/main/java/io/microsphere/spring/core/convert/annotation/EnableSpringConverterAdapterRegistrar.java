077c5e0dbb0ea6f5f778087275c911eee34a0f48
Mercy <mercyblitz@gmail.com>
2025-07-31 11:14:53 +0800

    Update EnableSpringConverterAdapterRegistrar.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/core/convert/annotation/EnableSpringConverterAdapterRegistrar.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/core/convert/annotation/EnableSpringConverterAdapterRegistrar.java
@@ -29,6 +29,7 @@ import org.springframework.core.convert.converter.ConverterRegistry;
 import org.springframework.core.type.AnnotationMetadata;
 
 import static io.microsphere.spring.beans.factory.BeanFactoryUtils.asConfigurableBeanFactory;
+import static io.microsphere.util.ArrayUtils.EMPTY_STRING_ARRAY;
 
 /**
  * {@link EnableSpringConverterAdapter} {@link Configuration} class
@@ -42,7 +43,7 @@ class EnableSpringConverterAdapterRegistrar implements ImportSelector, BeanFacto
 
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[0];
+        return EMPTY_STRING_ARRAY;
     }
 
     @Override
