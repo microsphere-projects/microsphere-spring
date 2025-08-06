a9afeed55783ca05daf69664edef6abcdc742235
Mercy Ma <mercyblitz@gmail.com>
2025-08-06 19:57:39 +0800

    Create EnableEmbeddedDatabaseTest.java

---
+++ microsphere-spring-test/src/test/java/io/microsphere/spring/test/jdbc/embedded/EnableEmbeddedDatabaseTest.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.test.jdbc.embedded;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+;
+
+/**
+ * {@link EnableEmbeddedDatabase} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @since 1.0.0
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = EnableEmbeddedDatabaseTest.class)
+@EnableEmbeddedDatabase(
+        dataSource = "primary",
+        primary = true,
+        properties = {
+                "username=admin"
+        })
+public class EnableEmbeddedDatabaseTest {
+
+    @Autowired
+    @Qualifier("primary")
+    private DataSource dataSource;
+
+    @Test
+    public void test() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        Statement statement = connection.createStatement();
+        statement.close();
+        connection.close();
+    }
+}
