7d7d59dc5b10327d1032de909472e35494bb7238
Mercy Ma <mercyblitz@gmail.com>
2025-08-06 11:24:13 +0800

    Update AutowireCandidateResolvingListener.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/beans/factory/support/AutowireCandidateResolvingListener.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/beans/factory/support/AutowireCandidateResolvingListener.java
@@ -39,6 +39,23 @@ import static org.springframework.core.annotation.AnnotationAwareOrderComparator
  *     <li>{@link AutowireCandidateResolver#getLazyResolutionProxyIfNecessary(DependencyDescriptor, String) resolving lazy proxy}</li>
  * </ul>
  *
+ * <h3>Example Usage</h3>
+ *
+ * Implementing this interface to handle events when a suggested value is resolved:
+ * <pre>{@code
+ * public class MyAutowireCandidateResolvingListener implements AutowireCandidateResolvingListener {
+ *     @Override
+ *     public void suggestedValueResolved(DependencyDescriptor descriptor, Object suggestedValue) {
+ *         System.out.println("Suggested value for " + descriptor + " is: " + suggestedValue);
+ *     }
+ *
+ *     @Override
+ *     public void lazyProxyResolved(DependencyDescriptor descriptor, String beanName, Object proxy) {
+ *         System.out.println("Lazy proxy for " + descriptor + " in bean " + beanName + " is: " + proxy);
+ *     }
+ * }
+ * }</pre>
+ *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see ListenableAutowireCandidateResolver
  * @see LoggingAutowireCandidateResolvingListener
