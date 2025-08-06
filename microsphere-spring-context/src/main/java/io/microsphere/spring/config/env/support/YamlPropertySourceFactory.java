8a8181866c79b516d30e2388f30504d4c9a81b1a
Mercy Ma <mercyblitz@gmail.com>
2025-08-06 17:36:21 +0800

    Update YamlPropertySourceFactory.java

--- microsphere-spring-context/src/main/java/io/microsphere/spring/config/env/support/YamlPropertySourceFactory.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/config/env/support/YamlPropertySourceFactory.java
@@ -17,6 +17,7 @@
 package io.microsphere.spring.config.env.support;
 
 import io.microsphere.spring.config.env.ImmutableMapPropertySource;
+import io.microsphere.spring.config.env.annotation.YamlPropertySource;
 import io.microsphere.spring.config.env.config.ResourceYamlProcessor;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
@@ -25,11 +26,26 @@ import org.springframework.core.io.support.PropertySourceFactory;
 import java.io.IOException;
 
 /**
- * {@link PropertySourceFactory} for YAML
+ * A {@link PropertySourceFactory} implementation that creates {@link PropertySource} instances from YAML resources.
+ *
+ * <p>This factory processes YAML files into a {@link java.util.Map} using {@link ResourceYamlProcessor},
+ * and wraps the result in an {@link ImmutableMapPropertySource} to ensure immutability.</p>
+ *
+ * <h3>Example Usage</h3>
+ * <pre>{@code
+ * // Configure a Spring Environment to use this factory for loading YAML resources
+ * ConfigurableEnvironment environment = context.getEnvironment();
+ * environment.setPropertySources(new YamlPropertySourceFactory().createPropertySource("my-config", encodedResource));
+ * }</pre>
+ *
+ * <p>For more information on how YAML resources are processed, see {@link ResourceYamlProcessor}.</p>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see YamlPropertySource
  * @see PropertySourceFactory
  * @see PropertySource
+ * @see ResourceYamlProcessor
+ * @see ImmutableMapPropertySource
  * @since 1.0.0
  */
 public class YamlPropertySourceFactory implements PropertySourceFactory {
