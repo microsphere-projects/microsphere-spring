e1bed547d0615811e4566d0eb733a05199e7812a
Mercy Ma <mercyblitz@gmail.com>
2025-08-06 20:01:16 +0800

    Update ConfigurationPropertyTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/config/ConfigurationPropertyTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/config/ConfigurationPropertyTest.java
@@ -20,12 +20,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * {@link ConfigurationProperty} Test
