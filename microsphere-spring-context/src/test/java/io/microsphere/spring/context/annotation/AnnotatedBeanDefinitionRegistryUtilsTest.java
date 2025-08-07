3c9f0b6de03876b6d6f8b63ac8f1e851b6430a10
Mercy Ma <mercyblitz@gmail.com>
2025-08-07 18:49:41 +0800

    Update AnnotatedBeanDefinitionRegistryUtilsTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/context/annotation/AnnotatedBeanDefinitionRegistryUtilsTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/annotation/AnnotatedBeanDefinitionRegistryUtilsTest.java
@@ -18,9 +18,9 @@ import static org.springframework.util.ObjectUtils.isEmpty;
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see AnnotatedBeanDefinitionRegistryUtils
- * @since 2017.01.13
+ * @since 1.0.0
  */
-public class AnnotatedBeanDefinitionRegistryUtilsTest {
+class AnnotatedBeanDefinitionRegistryUtilsTest {
 
     private DefaultListableBeanFactory registry = null;
 
