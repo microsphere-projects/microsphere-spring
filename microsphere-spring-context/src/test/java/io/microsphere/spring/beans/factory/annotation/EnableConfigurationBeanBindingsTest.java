616652666780321a58173ef77c7a3c1aeb635f09
Mercy Ma <mercyblitz@gmail.com>
2025-08-07 15:30:53 +0800

    Update EnableConfigurationBeanBindingsTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/annotation/EnableConfigurationBeanBindingsTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/annotation/EnableConfigurationBeanBindingsTest.java
@@ -16,7 +16,7 @@
  */
 package io.microsphere.spring.beans.factory.annotation;
 
-import io.microsphere.spring.util.User;
+import io.microsphere.spring.test.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Configuration;
 
