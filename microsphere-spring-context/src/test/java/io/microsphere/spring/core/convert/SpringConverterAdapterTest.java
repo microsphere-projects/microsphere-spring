9afd4a5bbfe6787e64776fa35446e82392f926e9
Mercy Ma <mercyblitz@gmail.com>
2025-08-07 18:51:22 +0800

    Update SpringConverterAdapterTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/core/convert/SpringConverterAdapterTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/core/convert/SpringConverterAdapterTest.java
@@ -38,9 +38,9 @@ import static org.springframework.core.convert.TypeDescriptor.valueOf;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public class SpringConverterAdapterTest {
+class SpringConverterAdapterTest {
 
-    private SpringConverterAdapter converterAdapter = SpringConverterAdapter.INSTANCE;
+    private final SpringConverterAdapter converterAdapter = SpringConverterAdapter.INSTANCE;
 
     @Test
     public void testMatch() {
@@ -52,7 +52,7 @@ public class SpringConverterAdapterTest {
         assertMatch(String.class, Short.class);
         assertMatch(String.class, Integer.class);
         assertMatch(String.class, Long.class);
-        assertMatch(String.class, Optional.class);
+        assertMatch(Object.class, Optional.class);
         assertMatch(String.class, String.class);
         assertMatch(Map.class, Properties.class);
         assertMatch(Properties.class, String.class);
@@ -68,7 +68,7 @@ public class SpringConverterAdapterTest {
         assertGetConvertibleTypes(String.class, Short.class);
         assertGetConvertibleTypes(String.class, Integer.class);
         assertGetConvertibleTypes(String.class, Long.class);
-        assertGetConvertibleTypes(String.class, Optional.class);
+        assertGetConvertibleTypes(Object.class, Optional.class);
         assertGetConvertibleTypes(String.class, String.class);
         assertGetConvertibleTypes(Map.class, Properties.class);
         assertGetConvertibleTypes(Properties.class, String.class);
