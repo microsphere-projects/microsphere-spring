f997605b27c7431b53813ca12f9562cb93b0dbac
Mercy Ma <mercyblitz@gmail.com>
2025-08-07 18:50:28 +0800

    Update ApplicationContextUtilsTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/context/ApplicationContextUtilsTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/ApplicationContextUtilsTest.java
@@ -38,7 +38,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
  * @see ApplicationContextUtils
  * @since 1.0.0
  */
-public class ApplicationContextUtilsTest {
+class ApplicationContextUtilsTest {
 
     private GenericApplicationContext context;
 
