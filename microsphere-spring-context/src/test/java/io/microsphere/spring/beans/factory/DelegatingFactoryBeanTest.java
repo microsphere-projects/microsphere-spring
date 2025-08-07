c2495ff1cc2e11b11efe955c02016afd1158fcc5
Mercy Ma <mercyblitz@gmail.com>
2025-08-07 18:57:27 +0800

    Update DelegatingFactoryBeanTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/DelegatingFactoryBeanTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/DelegatingFactoryBeanTest.java
@@ -24,9 +24,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.DisposableBean;
 
 import static io.microsphere.logging.LoggerFactory.getLogger;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * {@link DelegatingFactoryBean} Test
