2cb8f575d6991837a38817c34d52c176abf7883e
Mercy <mercyblitz@gmail.com>
2025-08-12 22:28:04 +0800

    Create ServletWebEndpointMappingResolverTest.java

---
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/metadata/ServletWebEndpointMappingResolverTest.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.web.metadata;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * {@link ServletWebEndpointMappingResolver} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ServletWebEndpointMappingResolver
+ * @since 1.0.0
+ */
+public class ServletWebEndpointMappingResolverTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testResolve() {
+    }
+}
