f3d9f04f334d800868611e550d3ad8e9e0b0eec4
Mercy <mercyblitz@gmail.com>
2025-08-13 13:20:22 +0800

    Update ServletWebEndpointMappingResolverTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/metadata/ServletWebEndpointMappingResolverTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/metadata/ServletWebEndpointMappingResolverTest.java
@@ -18,8 +18,28 @@
 package io.microsphere.spring.web.metadata;
 
 
+import io.microsphere.spring.test.web.servlet.TestServletContext;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+import javax.servlet.FilterRegistration;
+import java.util.Collection;
+import java.util.Iterator;
+
+import static io.microsphere.spring.test.util.ServletTestUtils.addTestFilter;
+import static io.microsphere.spring.test.util.ServletTestUtils.addTestServlet;
+import static io.microsphere.spring.test.web.servlet.TestFilter.DEFAULT_FILTER_URL_PATTERN;
+import static io.microsphere.spring.test.web.servlet.TestServlet.DEFAULT_SERVLET_URL_PATTERN;
+import static io.microsphere.spring.web.metadata.WebEndpointMapping.Kind.FILTER;
+import static io.microsphere.spring.web.metadata.WebEndpointMapping.Kind.SERVLET;
+import static io.microsphere.util.ArrayUtils.ofArray;
+import static java.util.EnumSet.of;
+import static javax.servlet.DispatcherType.REQUEST;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 /**
  * {@link ServletWebEndpointMappingResolver} Test
@@ -30,11 +50,39 @@ import org.junit.Test;
  */
 public class ServletWebEndpointMappingResolverTest {
 
+    private GenericWebApplicationContext context;
+
+    private ServletWebEndpointMappingResolver webEndpointMappingResolver;
+
     @Before
     public void setUp() throws Exception {
+        TestServletContext servletContext = new TestServletContext();
+        this.context = new GenericWebApplicationContext(servletContext);
+
+        addTestServlet(servletContext);
+
+        FilterRegistration.Dynamic filterRegistration = addTestFilter(servletContext);
+        String notFoundServletName = "notFoundServlet";
+        filterRegistration.addMappingForServletNames(of(REQUEST), true, notFoundServletName);
+
+        this.webEndpointMappingResolver = new ServletWebEndpointMappingResolver();
     }
 
     @Test
     public void testResolve() {
+        Collection<WebEndpointMapping> webEndpointMappings = webEndpointMappingResolver.resolve(this.context);
+        assertEquals(2, webEndpointMappings.size());
+
+        Iterator<WebEndpointMapping> iterator = webEndpointMappings.iterator();
+
+        assertTrue(iterator.hasNext());
+        WebEndpointMapping filterWebEndpointMapping = iterator.next();
+        assertSame(FILTER, filterWebEndpointMapping.getKind());
+        assertArrayEquals(ofArray(DEFAULT_FILTER_URL_PATTERN, DEFAULT_SERVLET_URL_PATTERN), filterWebEndpointMapping.getPatterns());
+
+        assertTrue(iterator.hasNext());
+        WebEndpointMapping servletWebEndpointMapping = iterator.next();
+        assertSame(SERVLET, servletWebEndpointMapping.getKind());
+        assertArrayEquals(ofArray(DEFAULT_SERVLET_URL_PATTERN), servletWebEndpointMapping.getPatterns());
     }
 }
