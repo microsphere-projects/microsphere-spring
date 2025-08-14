cff3fce6c91b9239b00ffb74ac3c739972d8eed8
Mercy <mercyblitz@gmail.com>
2025-08-14 17:34:44 +0800

    Create PersonHandler.java

---
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/test/PersonHandler.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.webflux.test;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * Person Handler
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ServerRequest
+ * @see ServerResponse
+ * @since 1.0.0
+ */
+public class PersonHandler {
+
+    public Mono<ServerResponse> listPeople(ServerRequest request) {
+        return Mono.empty();
+    }
+
+    public Mono<ServerResponse> createPerson(ServerRequest request) {
+        return Mono.empty();
+    }
+
+    public Mono<ServerResponse> getPerson(ServerRequest request) {
+        return Mono.empty();
+    }
+
+    public Mono<ServerResponse> updatePerson(ServerRequest request) {
+        return Mono.empty();
+    }
+
+    public Mono<ServerResponse> deletePerson(ServerRequest request) {
+        return Mono.empty();
+    }
+}
