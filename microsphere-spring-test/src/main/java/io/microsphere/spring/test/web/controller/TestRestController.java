60110bd37f193bcc59f8524393a1b93054cb38db
Mercy <mercyblitz@gmail.com>
2025-08-28 11:49:23 +0800

    Add error endpoint to TestRestController
    Introduced a new GET /error endpoint that throws a RuntimeException with a provided message. This can be used for testing error handling in the application.

--- microsphere-spring-test/src/main/java/io/microsphere/spring/test/web/controller/TestRestController.java
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/web/controller/TestRestController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -50,5 +51,10 @@ public class TestRestController {
     @PostMapping(path = "/user")
     public User user(@RequestBody User user) {
         return user;
+    }
+
+    @GetMapping("/error")
+    public String error(@RequestParam String message) {
+        throw new RuntimeException(message);
     }
 }
