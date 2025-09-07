f4ac87480c791f035970bc5b66c9872164c44445
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 22:12:32 +0800

    Update ListenableConfigurableEnvironmentInitializerTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/core/env/ListenableConfigurableEnvironmentInitializerTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/core/env/ListenableConfigurableEnvironmentInitializerTest.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.microsphere.spring.core.env;
 
 import org.junit.jupiter.api.Test;
