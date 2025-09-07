2e32e068c855ebd788c95e22ce4e22446da425ad
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:49:00 +0800

    Update RequestPredicateVisitorAdapterTest.java

--- microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/function/server/RequestPredicateVisitorAdapterTest.java
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/function/server/RequestPredicateVisitorAdapterTest.java
@@ -20,6 +20,34 @@ package io.microsphere.spring.webflux.function.server;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+
+import java.util.Set;
+
+import static io.microsphere.collection.Sets.ofSet;
+import static io.microsphere.spring.webflux.function.server.RequestPredicateKindTest.TEST_PATH_EXTENSION;
+import static io.microsphere.spring.webflux.test.WebTestUtils.PARAM_NAME;
+import static io.microsphere.spring.webflux.test.WebTestUtils.PARAM_VALUE;
+import static io.microsphere.spring.webflux.test.WebTestUtils.TEST_ROOT_PATH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_PDF;
+import static org.springframework.http.MediaType.APPLICATION_PDF_VALUE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.all;
+import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
+import static org.springframework.web.reactive.function.server.RequestPredicates.headers;
+import static org.springframework.web.reactive.function.server.RequestPredicates.method;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RequestPredicates.pathExtension;
+import static org.springframework.web.reactive.function.server.RequestPredicates.queryParam;
 
 /**
  * {@link RequestPredicateVisitorAdapter} Test
@@ -107,4 +135,122 @@ class RequestPredicateVisitorAdapterTest {
     void testUnknown() {
         this.adapter.unknown(null);
     }
+
+    @Test
+    void testVisit() {
+        RequestPredicate methodPredicate = method(GET);
+        RequestPredicate pathPredicate = path(TEST_ROOT_PATH);
+        RequestPredicate pathExtensionPredicate = pathExtension(TEST_PATH_EXTENSION);
+        RequestPredicate headersPredicate = headers(headers -> true);
+        RequestPredicate acceptPredicate = accept(APPLICATION_JSON);
+        RequestPredicate contentTypePredicate = contentType(APPLICATION_PDF);
+        RequestPredicate queryParamPredicate = queryParam(PARAM_NAME, PARAM_VALUE);
+        RequestPredicate andPredicate = methodPredicate.and(pathPredicate);
+        RequestPredicate orPredicate = methodPredicate.or(pathPredicate);
+        RequestPredicate negatePredicate = methodPredicate.negate();
+        RequestPredicate unknownPredicate = all();
+
+        ThreadLocal<RequestPredicate> threadLocal = new ThreadLocal<>();
+
+        this.adapter = new RequestPredicateVisitorAdapter() {
+
+            @Override
+            public void method(Set<HttpMethod> methods) {
+                assertEquals(ofSet(GET), methods);
+            }
+
+            @Override
+            public void path(String pattern) {
+                assertEquals(TEST_ROOT_PATH, pattern);
+            }
+
+            @Override
+            public void pathExtension(String extension) {
+                assertEquals(TEST_PATH_EXTENSION, extension);
+            }
+
+            @Override
+            public void header(String name, String value) {
+                if (ACCEPT.equals(name)) {
+                    assertEquals(APPLICATION_JSON_VALUE, value);
+                } else if (CONTENT_TYPE.equals(name)) {
+                    assertEquals(APPLICATION_PDF_VALUE, value);
+                }
+            }
+
+            @Override
+            public void queryParam(String name, String value) {
+                assertEquals(PARAM_NAME, name);
+                assertEquals(PARAM_VALUE, value);
+            }
+
+            @Override
+            public void startAnd() {
+                assertSame(threadLocal.get(), andPredicate);
+            }
+
+            @Override
+            public void and() {
+                assertSame(threadLocal.get(), andPredicate);
+            }
+
+            @Override
+            public void endAnd() {
+                assertSame(threadLocal.get(), andPredicate);
+            }
+
+            @Override
+            public void startOr() {
+                assertSame(threadLocal.get(), orPredicate);
+            }
+
+            @Override
+            public void or() {
+                assertSame(threadLocal.get(), orPredicate);
+            }
+
+            @Override
+            public void endOr() {
+                assertSame(threadLocal.get(), orPredicate);
+            }
+
+            @Override
+            public void startNegate() {
+                assertSame(threadLocal.get(), negatePredicate);
+            }
+
+            @Override
+            public void endNegate() {
+                assertSame(threadLocal.get(), negatePredicate);
+            }
+
+            @Override
+            public void unknown(RequestPredicate predicate) {
+                assertNotNull(predicate);
+            }
+        };
+
+        this.adapter.visit(methodPredicate);
+        this.adapter.visit(pathPredicate);
+        this.adapter.visit(pathExtensionPredicate);
+        this.adapter.visit(headersPredicate);
+        this.adapter.visit(acceptPredicate);
+        this.adapter.visit(contentTypePredicate);
+        this.adapter.visit(queryParamPredicate);
+
+        threadLocal.set(andPredicate);
+        this.adapter.visit(andPredicate);
+        threadLocal.remove();
+
+        threadLocal.set(orPredicate);
+        this.adapter.visit(orPredicate);
+        threadLocal.remove();
+
+        threadLocal.set(negatePredicate);
+        this.adapter.visit(negatePredicate);
+        threadLocal.remove();
+
+        this.adapter.visit(unknownPredicate);
+    }
+
 }
