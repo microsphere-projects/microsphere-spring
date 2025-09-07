d4d2b2bf8a9cf26137da422c8af8719c66cf9f69
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:47:30 +0800

    Update WebEndpointMappingFactoryTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/metadata/WebEndpointMappingFactoryTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/metadata/WebEndpointMappingFactoryTest.java
@@ -33,7 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @see WebEndpointMappingFactory
  * @since 1.0.0
  */
-public class WebEndpointMappingFactoryTest {
+class WebEndpointMappingFactoryTest {
 
     private WebEndpointMappingFactory factory;
 
