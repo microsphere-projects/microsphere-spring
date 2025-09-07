47e87a3e82464edc29859092fd046505c9dd6f46
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:49:40 +0800

    Update EnableWebMvcExtensionInterceptorsTest.java

--- microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtensionInterceptorsTest.java
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtensionInterceptorsTest.java
@@ -39,7 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @EnableWebMvcExtension(handlerInterceptors = {
         IdempotentAnnotatedMethodHandlerInterceptor.class
 })
-public class EnableWebMvcExtensionInterceptorsTest extends AbstractEnableWebMvcExtensionTest {
+class EnableWebMvcExtensionInterceptorsTest extends AbstractEnableWebMvcExtensionTest {
 
     @Test
     @Override
