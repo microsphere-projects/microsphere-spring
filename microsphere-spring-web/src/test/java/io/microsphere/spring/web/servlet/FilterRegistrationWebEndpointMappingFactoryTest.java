6f92528cd5ddbef4c53e6c7b08982359cfdf746d
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:48:25 +0800

    Update FilterRegistrationWebEndpointMappingFactoryTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/servlet/FilterRegistrationWebEndpointMappingFactoryTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/servlet/FilterRegistrationWebEndpointMappingFactoryTest.java
@@ -37,7 +37,7 @@
 // * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
 // * @since 1.0.0
 // */
-//public class FilterRegistrationWebEndpointMappingFactoryTest {
+//class FilterRegistrationWebEndpointMappingFactoryTest {
 //
 //    private FilterRegistrationWebEndpointMappingFactory factory;
 //
