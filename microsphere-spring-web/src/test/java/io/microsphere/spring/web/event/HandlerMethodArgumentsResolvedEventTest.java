a9299bfcf714c167bc765e714e4c676cc0f98993
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:47:00 +0800

    Update HandlerMethodArgumentsResolvedEventTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/event/HandlerMethodArgumentsResolvedEventTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/event/HandlerMethodArgumentsResolvedEventTest.java
@@ -39,7 +39,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
  * @see HandlerMethodArgumentsResolvedEvent
  * @since 1.0.0
  */
-public class HandlerMethodArgumentsResolvedEventTest {
+class HandlerMethodArgumentsResolvedEventTest {
 
     private HandlerMethod handlerMethod;
 
