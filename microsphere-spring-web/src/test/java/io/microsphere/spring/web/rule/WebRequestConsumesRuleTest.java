1908e7b277919e1c430baec83147082233d48325
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:48:08 +0800

    Update WebRequestConsumesRuleTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestConsumesRuleTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestConsumesRuleTest.java
@@ -34,7 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @see WebRequestConsumesRule
  * @since 1.0.0
  */
-public class WebRequestConsumesRuleTest extends BaseWebRequestRuleTest {
+class WebRequestConsumesRuleTest extends BaseWebRequestRuleTest {
 
     // Test when request is preflight (should return false)
     @Test
