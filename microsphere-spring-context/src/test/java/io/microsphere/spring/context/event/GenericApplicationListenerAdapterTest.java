37df06bea6552d0267346e83b6b9b117c2953efa
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 18:23:34 +0800

    Update GenericApplicationListenerAdapterTest.java

--- microsphere-spring-context/src/test/java/io/microsphere/spring/context/event/GenericApplicationListenerAdapterTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/event/GenericApplicationListenerAdapterTest.java
@@ -43,7 +43,7 @@ import static org.springframework.core.ResolvableType.forClass;
         classes = {
                 GenericApplicationListenerAdapterTest.GenericApplicationListenerAdapterImpl.class
         })
-public class GenericApplicationListenerAdapterTest {
+class GenericApplicationListenerAdapterTest {
 
     static class GenericApplicationListenerAdapterImpl implements GenericApplicationListenerAdapter {
 
