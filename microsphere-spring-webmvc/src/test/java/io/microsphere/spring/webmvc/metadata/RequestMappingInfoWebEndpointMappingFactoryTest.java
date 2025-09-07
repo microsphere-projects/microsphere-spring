82fe584794ec8af302894229d3915b4021a45f58
Mercy Ma <mercyblitz@gmail.com>
2025-09-07 19:49:51 +0800

    Update RequestMappingInfoWebEndpointMappingFactoryTest.java

--- microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/metadata/RequestMappingInfoWebEndpointMappingFactoryTest.java
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/metadata/RequestMappingInfoWebEndpointMappingFactoryTest.java
@@ -33,6 +33,7 @@ import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMappi
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -52,7 +53,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
         RequestMappingInfoWebEndpointMappingFactoryTest.class,
         RequestMappingInfoWebEndpointMappingFactoryTest.Config.class
 })
-public class RequestMappingInfoWebEndpointMappingFactoryTest {
+class RequestMappingInfoWebEndpointMappingFactoryTest {
 
     private Map<RequestMappingInfo, HandlerMethod> handlerMethods;
 
@@ -81,7 +82,7 @@ public class RequestMappingInfoWebEndpointMappingFactoryTest {
     @Test
     void test() {
         assertNotNull(handlerMethods);
-        for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
+        for (Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
             RequestMappingMetadata metadata = new RequestMappingMetadata(entry.getKey(), entry.getValue());
             Optional<WebEndpointMapping<HandlerMetadata<HandlerMethod, RequestMappingInfo>>> webEndpointMapping = factory.create(metadata);
             assertTrue(webEndpointMapping.isPresent());
