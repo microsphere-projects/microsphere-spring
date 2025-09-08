641f1f677a0a015113c7fb7028626d58e12ee3a5
Mercy Ma <mercyblitz@gmail.com>
2025-09-08 17:24:05 +0800

    Create ExclusiveViewResolverApplicationListenerTestOnViewResolverCompositeNotAvailable.java

---
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/context/ExclusiveViewResolverApplicationListenerTestOnViewResolverCompositeNotAvailable.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.webmvc.context;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.ViewResolverComposite;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * {@link ExclusiveViewResolverApplicationListener} Test on {@link ViewResolverComposite} not available
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ExclusiveViewResolverApplicationListener
+ * @since 1.0.0
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        ViewResolverConfig.class,
+        ExclusiveViewResolverApplicationListener.class,
+        ExclusiveViewResolverApplicationListenerTestOnViewResolverCompositeNotAvailable.class
+})
+@TestPropertySource(
+        properties = {
+                "microsphere.spring.webmvc.view-resolver.exclusive-bean-name=beanNameViewResolver"
+        }
+)
+class ExclusiveViewResolverApplicationListenerTestOnViewResolverCompositeNotAvailable {
+
+
+    @Autowired
+    private BeanNameViewResolver beanNameViewResolver;
+
+    @Test
+    void test() {
+        assertNotNull(beanNameViewResolver);
+    }
+}
