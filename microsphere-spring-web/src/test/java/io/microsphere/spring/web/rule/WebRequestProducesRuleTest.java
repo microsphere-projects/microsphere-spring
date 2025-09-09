3f58a94caf2e5aabe303deab979d628586f16988
Mercy Ma <mercyblitz@gmail.com>
2025-09-09 14:36:45 +0800

    Update WebRequestProducesRuleTest.java

--- microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestProducesRuleTest.java
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/rule/WebRequestProducesRuleTest.java
@@ -35,6 +35,7 @@ import static io.microsphere.spring.test.util.SpringTestWebUtils.createWebReques
 import static java.util.Collections.singletonMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -77,17 +78,26 @@ class WebRequestProducesRuleTest extends BaseWebRequestRuleTest {
 
     @Override
     protected void doTestEquals() {
+        WebRequestProducesRule rule = new WebRequestProducesRule(APPLICATION_JSON_VALUE);
 
+        assertEquals(rule, rule);
+        assertEquals(rule, new WebRequestProducesRule(APPLICATION_JSON_VALUE));
+
+        assertNotEquals(rule, new WebRequestPattensRule(APPLICATION_XML_VALUE));
+        assertNotEquals(rule, this);
+        assertNotEquals(rule, null);
     }
 
     @Override
     protected void doTestHashCode() {
-
+        WebRequestProducesRule rule = new WebRequestProducesRule(APPLICATION_JSON_VALUE);
+        assertEquals(rule.hashCode(), rule.getContent().hashCode());
     }
 
     @Override
     protected void doTestToString() {
-
+        WebRequestProducesRule rule = new WebRequestProducesRule(APPLICATION_JSON_VALUE);
+        assertEquals("[application/json]", rule.toString());
     }
 
     // Test pre-flight request returns false
