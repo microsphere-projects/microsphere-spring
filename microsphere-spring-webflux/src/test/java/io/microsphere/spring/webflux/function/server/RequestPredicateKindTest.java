1e6c498784e2565654db36184fbf7c055ca9d0d4
Mercy <mercyblitz@gmail.com>
2025-09-09 13:46:15 +0800

    Update RequestPredicateKindTest.java

--- microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/function/server/RequestPredicateKindTest.java
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/function/server/RequestPredicateKindTest.java
@@ -352,7 +352,7 @@ class RequestPredicateKindTest {
 
     @Test
     void testMatchesWithExpressionOnHeaders() {
-        assertFalse(HEADERS.matches("Accept"));
+        assertFalse(HEADERS.matches(ACCEPT));
     }
 
     @Test
