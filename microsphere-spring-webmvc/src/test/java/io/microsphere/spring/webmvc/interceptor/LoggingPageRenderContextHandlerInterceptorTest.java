cb03f50af1832cc2fc0ffd0c7cc8649358b2d794
Mercy Ma <mercyblitz@gmail.com>
2025-09-10 20:28:57 +0800

    Create LoggingPageRenderContextHandlerInterceptorTest.java

---
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/interceptor/LoggingPageRenderContextHandlerInterceptorTest.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.webmvc.interceptor;
+
+
+import io.microsphere.spring.test.web.controller.TestController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import static io.microsphere.reflect.MethodUtils.findMethod;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * {@link LoggingPageRenderContextHandlerInterceptor} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see LoggingPageRenderContextHandlerInterceptor
+ * @since 1.0.0
+ */
+class LoggingPageRenderContextHandlerInterceptorTest {
+
+    private LoggingPageRenderContextHandlerInterceptor interceptor;
+
+    private HttpServletRequest request;
+
+    private HttpServletResponse response;
+
+    private Object handler;
+
+    private ModelAndView modelAndView;
+
+    @BeforeEach
+    void setUp() {
+        this.interceptor = new LoggingPageRenderContextHandlerInterceptor();
+        this.request = new MockHttpServletRequest();
+        this.response = new MockHttpServletResponse();
+        TestController testController = new TestController();
+        this.handler = new HandlerMethod(testController, findMethod(TestController.class, "helloWorld"));
+        this.modelAndView = new ModelAndView();
+        this.modelAndView.setViewName("test-view");
+        this.modelAndView.addObject("name", "value");
+    }
+
+    @Test
+    void testPostHandle() throws Exception {
+        this.interceptor.postHandle(this.request, this.response, this.handler, null);
+    }
+
+    @Test
+    void testPreHandle() throws Exception {
+        assertTrue(this.interceptor.preHandle(this.request, this.response, this.handler));
+    }
+
+    @Test
+    void testAfterCompletion() throws Exception {
+        this.interceptor.afterCompletion(this.request, this.response, this.handler, null);
+    }
+
+    @Test
+    void testPostHandleOnPageRenderContext() throws Exception {
+        this.interceptor.postHandleOnPageRenderContext(this.request, this.response, this.handler, this.modelAndView);
+    }
+}
