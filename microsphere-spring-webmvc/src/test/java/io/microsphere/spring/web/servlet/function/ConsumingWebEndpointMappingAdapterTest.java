9edd29e57de420f299a6cb78817eea4b4432a17f
Mercy Ma <mercyblitz@gmail.com>
2025-09-11 12:10:20 +0800

    Create ConsumingWebEndpointMappingAdapterTest.java

---
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/web/servlet/function/ConsumingWebEndpointMappingAdapterTest.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.web.servlet.function;
+
+import io.microsphere.spring.test.web.controller.TestController;
+import io.microsphere.spring.web.metadata.WebEndpointMapping;
+import io.microsphere.spring.webmvc.test.AbstractWebMvcTest;
+import io.microsphere.spring.webmvc.test.RouterFunctionTestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.support.RouterFunctionMapping;
+
+import java.util.Collection;
+
+import static io.microsphere.collection.ListUtils.newLinkedList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link ConsumingWebEndpointMappingAdapter} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ConsumingWebEndpointMappingAdapter
+ * @since 1.0.0
+ */
+@ContextConfiguration(classes = {
+        ConsumingWebEndpointMappingAdapterTest.class,
+        RouterFunctionTestConfig.class
+})
+@Import(TestController.class)
+public class ConsumingWebEndpointMappingAdapterTest extends AbstractWebMvcTest {
+
+    @Test
+    void test() {
+        RouterFunctionMapping routerFunctionMapping = context.getBean(RouterFunctionMapping.class);
+        RouterFunction<?> routerFunction = routerFunctionMapping.getRouterFunction();
+        Collection<WebEndpointMapping> webEndpointMappings = newLinkedList();
+        routerFunction.accept(new ConsumingWebEndpointMappingAdapter(webEndpointMappings::add));
+        assertEquals(5, webEndpointMappings.size());
+    }
+
+}
+
