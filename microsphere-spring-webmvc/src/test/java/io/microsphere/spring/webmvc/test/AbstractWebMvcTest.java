2002a81dd25374b530c35a9bb57843414624bcc2
Mercy Ma <mercyblitz@gmail.com>
2025-09-11 12:09:27 +0800

    Create AbstractWebMvcTest.java

---
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/test/AbstractWebMvcTest.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.webmvc.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * Abstract class for WebMVC testing
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see EnableWebMvc
+ * @since 1.0.0
+ */
+@SpringJUnitConfig
+@WebAppConfiguration
+@EnableWebMvc
+@Disabled
+public abstract class AbstractWebMvcTest {
+
+    @Autowired
+    protected ConfigurableWebApplicationContext context;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = webAppContextSetup(this.context).build();
+    }
+
+}
