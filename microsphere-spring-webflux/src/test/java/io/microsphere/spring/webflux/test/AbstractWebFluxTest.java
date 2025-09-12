78b15666cc05b75a2ee182467848bbb568f2d4ad
Mercy <mercyblitz@gmail.com>
2025-09-12 20:33:19 +0800

    Update AbstractWebFluxTest.java

--- microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/test/AbstractWebFluxTest.java
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/test/AbstractWebFluxTest.java
@@ -16,16 +16,22 @@
  */
 package io.microsphere.spring.webflux.test;
 
+import io.microsphere.spring.test.domain.User;
+import io.microsphere.spring.test.web.controller.TestController;
 import io.microsphere.spring.webflux.annotation.EnableWebFluxExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Mono;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.reactive.server.WebTestClient.bindToApplicationContext;
+import static reactor.core.publisher.Mono.just;
 
 /**
  * Abstract WebFlux Test
@@ -35,18 +41,112 @@ import static org.springframework.test.web.reactive.server.WebTestClient.bindToA
  * @since 1.0.0
  */
 @SpringJUnitConfig
-@EnableWebFlux
 @Disabled
+@EnableWebFlux
+@Import(value = {
+        TestController.class,           // Test Controller
+        RouterFunctionTestConfig.class  // Test RouterFunction
+})
 public abstract class AbstractWebFluxTest {
 
     @Autowired
     protected ConfigurableApplicationContext context;
+
+    @Autowired
+    protected TestController testController;
 
     protected WebTestClient webTestClient;
 
     @BeforeEach
     final void init() {
         this.webTestClient = buildWebTestClient(this.context);
+    }
+
+
+    /**
+     * Test {@link TestController#helloWorld()}
+     */
+    protected void testHelloWorld() {
+        this.webTestClient.get()
+                .uri("/test/helloworld")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .isEqualTo(this.testController.helloWorld());
+    }
+
+    /**
+     * Test {@link TestController#greeting(String)}
+     */
+    protected void testGreeting() {
+        String pattern = "/test/greeting/{message}";
+        String message = "Mercy";
+        this.webTestClient.get()
+                .uri(pattern, message)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo(this.testController.greeting(message));
+    }
+
+    /**
+     * Test {@link TestController#user(User)}
+     */
+    protected void testUser() {
+        String pattern = "/test/user";
+        User user = new User();
+        user.setName("Mercy");
+        user.setAge(18);
+
+        Mono<User> userMono = just(user);
+        this.webTestClient.post()
+                .uri(pattern)
+                .accept(APPLICATION_JSON)
+                .body(userMono, User.class)
+                .exchange()
+                .expectBody(User.class)
+                .isEqualTo(this.testController.user(user));
+    }
+
+    /**
+     * Test {@link TestController#error(String)}
+     */
+    protected void testError() {
+        this.webTestClient.get()
+                .uri("/test/error?message=hello")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError();
+    }
+
+    /**
+     * Test {@link TestController#responseEntity()}
+     */
+    protected void testResponseEntity() {
+        this.webTestClient.put()
+                .uri("/test/response-entity")
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo(this.testController.responseEntity().getBody());
+    }
+
+    /**
+     * Test {@link TestController#view()}
+     */
+    protected void testView() {
+        this.webTestClient.get()
+                .uri("/test/view")
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    /**
+     * @see RouterFunctionTestConfig#nestedPersonRouterFunction(PersonHandler)
+     */
+    protected void testUpdatePerson() {
+        this.webTestClient.put()
+                .uri("/test/person/{id}", "1")
+                .exchange()
+                .expectStatus().isOk();
     }
 
     public static WebTestClient buildWebTestClient(ConfigurableApplicationContext context) {
