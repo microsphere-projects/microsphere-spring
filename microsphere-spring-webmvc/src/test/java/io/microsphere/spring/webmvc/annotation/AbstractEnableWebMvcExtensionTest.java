5679f105d0616e2d31fb396bed0ad7136b6b0aca
Mercy <mercyblitz@gmail.com>
2025-09-13 19:23:39 +0800

    Update AbstractEnableWebMvcExtensionTest.java

--- microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/AbstractEnableWebMvcExtensionTest.java
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/AbstractEnableWebMvcExtensionTest.java
@@ -31,6 +31,7 @@ import io.microsphere.spring.webmvc.test.AbstractWebMvcTest;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static io.microsphere.spring.beans.BeanUtils.isBeanPresent;
 import static io.microsphere.util.ArrayUtils.isNotEmpty;
@@ -45,6 +46,7 @@ import static org.junit.Assert.assertTrue;
  * @since 1.0.0
  */
 @Ignore
+@EnableWebMvc
 public abstract class AbstractEnableWebMvcExtensionTest extends AbstractWebMvcTest {
 
     protected boolean registerWebEndpointMappings;
