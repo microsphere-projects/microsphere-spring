4c373d7f366f9934480357d0dae6c5c0071ccaa3
Mercy <mercyblitz@gmail.com>
2025-09-13 10:38:37 +0800

    Create LoggingMethodHandlerInterceptorTest.java

---
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/interceptor/LoggingMethodHandlerInterceptorTest.java
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.webmvc.interceptor;
+
+
+import io.microsphere.spring.test.web.controller.TestController;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static io.microsphere.reflect.MethodUtils.findMethod;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * {@link LoggingMethodHandlerInterceptor} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see LoggingMethodHandlerInterceptor
+ * @since 1.0.0
+ */
+public class LoggingMethodHandlerInterceptorTest {
+
+    private LoggingMethodHandlerInterceptor interceptor;
+
+    private HttpServletRequest request;
+
+    private HttpServletResponse response;
+
+    private HandlerMethod handlerMethod;
+
+    private ModelAndView modelAndView;
+
+    @Before
+    public void setUp() {
+        this.interceptor = new LoggingMethodHandlerInterceptor();
+        this.request = new MockHttpServletRequest();
+        this.response = new MockHttpServletResponse();
+        TestController testController = new TestController();
+        this.handlerMethod = new HandlerMethod(testController, findMethod(TestController.class, "helloWorld"));
+        this.modelAndView = new ModelAndView();
+        this.modelAndView.setViewName("test-view");
+        this.modelAndView.addObject("name", "value");
+    }
+
+    @Test
+    public void testPreHandle() throws Exception {
+        assertTrue(this.interceptor.preHandle(this.request, this.response, this.handlerMethod));
+        assertTrue(this.interceptor.preHandle(this.request, this.response, null));
+        assertTrue(this.interceptor.preHandle(this.request, this.response, (Object) null));
+    }
+
+    @Test
+    public void testPostHandle() throws Exception {
+        this.interceptor.postHandle(this.request, this.response, this.handlerMethod, modelAndView);
+        this.interceptor.postHandle(this.request, this.response, this.handlerMethod, null);
+        this.interceptor.postHandle(this.request, this.response, (Object) null, null);
+    }
+
+    @Test
+    public void testAfterCompletion() throws Exception {
+        this.interceptor.afterCompletion(this.request, this.response, this.handlerMethod, null);
+        this.interceptor.afterCompletion(this.request, this.response, this.handlerMethod, new Exception("For testing"));
+        this.interceptor.afterCompletion(this.request, this.response, (Object) null, null);
+    }
+
+    @Test
+    public void testSupports() throws Exception {
+        assertTrue(this.interceptor.supports(this.request, this.response, this.handlerMethod));
+    }
+}
