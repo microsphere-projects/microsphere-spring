6968f435bad6bdffb3829be2f8c0845853474540
Mercy Ma <mercyblitz@gmail.com>
2025-09-13 17:46:27 +0800

    Update AbstractEnableWebFluxExtensionTest.java

--- microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/annotation/AbstractEnableWebFluxExtensionTest.java
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/annotation/AbstractEnableWebFluxExtensionTest.java
@@ -16,16 +16,10 @@
  */
 package io.microsphere.spring.webflux.annotation;
 
-import io.microsphere.spring.test.web.controller.TestController;
-import io.microsphere.spring.web.event.HandlerMethodArgumentsResolvedEvent;
-import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
 import io.microsphere.spring.web.event.WebEventPublisher;
 import io.microsphere.spring.web.metadata.SimpleWebEndpointMappingRegistry;
-import io.microsphere.spring.web.metadata.WebEndpointMapping;
 import io.microsphere.spring.web.metadata.WebEndpointMappingRegistrar;
 import io.microsphere.spring.web.method.support.DelegatingHandlerMethodAdvice;
-import io.microsphere.spring.web.method.support.HandlerMethodArgumentInterceptor;
-import io.microsphere.spring.web.method.support.HandlerMethodInterceptor;
 import io.microsphere.spring.webflux.handler.ReversedProxyHandlerMapping;
 import io.microsphere.spring.webflux.metadata.HandlerMappingWebEndpointMappingResolver;
 import io.microsphere.spring.webflux.method.InterceptingHandlerMethodProcessor;
@@ -35,19 +29,9 @@ import io.microsphere.spring.webflux.test.AbstractWebFluxTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.MethodParameter;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.HandlerMethod;
-
-import java.lang.reflect.Method;
-import java.util.Collection;
 
 import static io.microsphere.spring.beans.BeanUtils.isBeanPresent;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Abstract {@link EnableWebFluxExtension} Test
@@ -57,8 +41,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @since 1.0.0
  */
 @Disabled
-public abstract class AbstractEnableWebFluxExtensionTest extends AbstractWebFluxTest implements
-        HandlerMethodArgumentInterceptor, HandlerMethodInterceptor {
+public abstract class AbstractEnableWebFluxExtensionTest extends AbstractWebFluxTest {
 
     protected boolean registerWebEndpointMappings;
 
@@ -119,69 +102,4 @@ public abstract class AbstractEnableWebFluxExtensionTest extends AbstractWebFlux
         this.testResponseEntity();
         this.testUpdatePerson();
     }
-
-    @EventListener(WebEndpointMappingsReadyEvent.class)
-    public void onWebEndpointMappingsReadyEvent(WebEndpointMappingsReadyEvent event) {
-        Collection<WebEndpointMapping> mappings = event.getMappings();
-        assertTrue(mappings.size() > 0);
-    }
-
-    @EventListener(HandlerMethodArgumentsResolvedEvent.class)
-    public void onHandlerMethodArgumentsResolvedEvent(HandlerMethodArgumentsResolvedEvent event) {
-        Method method = event.getMethod();
-        HandlerMethod handlerMethod = event.getHandlerMethod();
-        assertEquals(method, handlerMethod.getMethod());
-        assertHandlerMethod(handlerMethod);
-        Object[] arguments = event.getArguments();
-        assertArguments(method, arguments);
-    }
-
-    @Override
-    public void beforeResolveArgument(MethodParameter parameter, HandlerMethod handlerMethod, NativeWebRequest webRequest) {
-        assertHandlerMethod(handlerMethod);
-        assertNativeWebRequest(webRequest);
-    }
-
-    @Override
-    public void afterResolveArgument(MethodParameter parameter, Object resolvedArgument, HandlerMethod handlerMethod, NativeWebRequest webRequest) {
-        // Reuse
-        beforeResolveArgument(parameter, handlerMethod, webRequest);
-    }
-
-    @Override
-    public void beforeExecute(HandlerMethod handlerMethod, Object[] args, NativeWebRequest request) {
-        assertHandlerMethod(handlerMethod);
-        assertArguments(handlerMethod.getMethod(), args);
-    }
-
-    @Override
-    public void afterExecute(HandlerMethod handlerMethod, Object[] args, Object returnValue, Throwable error, NativeWebRequest request) {
-        beforeExecute(handlerMethod, args, request);
-        if (returnValue == null) {
-            assertNotNull(error);
-        } else {
-            assertReturnValue(returnValue);
-            assertNull(error);
-        }
-    }
-
-    protected void assertHandlerMethod(HandlerMethod handlerMethod) {
-        assertNotNull(handlerMethod);
-        Object bean = handlerMethod.getBean();
-        assertNotNull(bean);
-        assertEquals(TestController.class, handlerMethod.getBeanType());
-    }
-
-    protected void assertArguments(Method method, Object[] arguments) {
-        assertEquals(method.getParameterCount(), arguments.length);
-    }
-
-    protected void assertReturnValue(Object returnValue) {
-        assertNotNull(returnValue);
-    }
-
-    protected void assertNativeWebRequest(NativeWebRequest webRequest) {
-        assertNotNull(webRequest);
-    }
-
 }
