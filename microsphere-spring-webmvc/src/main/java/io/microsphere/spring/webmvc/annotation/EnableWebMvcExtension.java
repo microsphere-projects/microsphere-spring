d49bb3e4be01e25ca376579844b98d054ffc419c
Mercy Ma <mercyblitz@gmail.com>
2025-09-16 11:57:45 +0800

    Update EnableWebMvcExtension.java

--- microsphere-spring-webmvc/src/main/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtension.java
+++ microsphere-spring-webmvc/src/main/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtension.java
@@ -43,11 +43,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Method;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Enable annotation to extend the features of Spring WebMVC
@@ -59,8 +60,8 @@ import java.lang.reflect.Method;
  * @see WebMvcExtensionConfiguration
  * @since 1.0.0
  */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Retention(RUNTIME)
+@Target(TYPE)
 @Documented
 @Import(value = {
         WebMvcExtensionBeanDefinitionRegistrar.class,
