5bba63ba43e51020ecebea098976f3ff23bb5782
Mercy Ma <mercyblitz@gmail.com>
2025-09-16 21:18:30 +0800

    Create SpringWebTypeTest.java

---
+++ microsphere-spring-web/src/test/java/io/microsphere/spring/web/util/SpringWebTypeTest.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.web.util;
+
+
+import io.microsphere.spring.web.context.request.MockServletWebRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import static io.microsphere.spring.web.util.SpringWebType.WEBFLUX_INDICATOR_CLASS_NAME;
+import static io.microsphere.spring.web.util.SpringWebType.WEBMVC_INDICATOR_CLASS_NAME;
+import static io.microsphere.spring.web.util.SpringWebType.WEB_FLUX;
+import static io.microsphere.spring.web.util.SpringWebType.WEB_MVC;
+import static io.microsphere.spring.web.util.SpringWebType.valueOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+/**
+ * {@link SpringWebType} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see SpringWebType
+ * @since 1.0.0
+ */
+class SpringWebTypeTest {
+
+    @Test
+    void testConstants() {
+        assertEquals("org.springframework.web.servlet.DispatcherServlet", WEBMVC_INDICATOR_CLASS_NAME);
+        assertEquals("org.springframework.web.reactive.DispatcherHandler", WEBFLUX_INDICATOR_CLASS_NAME);
+    }
+
+    @Test
+    void testValueOf() {
+        assertSame(WEB_MVC, valueOf(new MockServletWebRequest()));
+        assertSame(WEB_FLUX, valueOf(mock(NativeWebRequest.class)));
+    }
+
+    @Test
+    void testValueOfWithNull() {
+        assertThrows(IllegalArgumentException.class, () -> valueOf((NativeWebRequest) null));
+    }
+}
