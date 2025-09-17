53408f1e7c14f9fd249db2ea761ec99254855510
Mercy Ma <mercyblitz@gmail.com>
2025-09-17 20:59:12 +0800

    Update WebRequestUtils.java

--- microsphere-spring-web/src/main/java/io/microsphere/spring/web/util/WebRequestUtils.java
+++ microsphere-spring-web/src/main/java/io/microsphere/spring/web/util/WebRequestUtils.java
@@ -16,15 +16,31 @@
  */
 package io.microsphere.spring.web.util;
 
+import io.microsphere.annotation.Nonnull;
 import io.microsphere.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.util.UrlPathHelper;
 
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static io.microsphere.spring.web.util.SpringWebType.valueOf;
+import static io.microsphere.spring.web.util.UnknownSpringWebHelper.INSTANCE;
+import static io.microsphere.spring.web.util.WebScope.REQUEST;
+import static io.microsphere.util.ClassLoaderUtils.getClassLoader;
+import static org.springframework.core.io.support.SpringFactoriesLoader.loadFactories;
 import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD;
 import static org.springframework.http.HttpHeaders.CONTENT_LENGTH;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
@@ -34,7 +50,6 @@ import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
 import static org.springframework.http.MediaType.parseMediaType;
 import static org.springframework.util.StringUtils.hasLength;
 import static org.springframework.util.StringUtils.hasText;
-import static org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST;
 
 /**
  * {@link WebRequest} Utilities class
@@ -43,6 +58,8 @@ import static org.springframework.web.context.request.RequestAttributes.SCOPE_RE
  * @since 1.0.0
  */
 public abstract class WebRequestUtils {
+
+    private static EnumMap<SpringWebType, SpringWebHelper> springWebHelpers = loadSpringWebHelpers();
 
     /**
      * The header name for HTTP method
@@ -57,11 +74,11 @@ public abstract class WebRequestUtils {
      */
     public static final String PATH_ATTRIBUTE = UrlPathHelper.class.getName() + ".PATH";
 
-    public static String getMethod(NativeWebRequest request) {
+    public static String getMethod(@Nonnull NativeWebRequest request) {
         String method = request.getHeader(METHOD_HEADER_NAME);
         if (method == null) {
-            Object nativeRequest = request.getNativeRequest();
-            if (nativeRequest instanceof HttpServletRequest) {
+            if (request instanceof ServletWebRequest) {
+                Object nativeRequest = request.getNativeRequest();
                 HttpServletRequest servletRequest = (HttpServletRequest) nativeRequest;
                 method = servletRequest.getMethod();
             }
@@ -73,19 +90,19 @@ public abstract class WebRequestUtils {
      * Returns {@code true} if the request is a valid CORS pre-flight one by checking {@code OPTIONS} method with
      * {@code Origin} and {@code Access-Control-Request-Method} headers presence.
      */
-    public static boolean isPreFlightRequest(NativeWebRequest request) {
+    public static boolean isPreFlightRequest(@Nonnull NativeWebRequest request) {
         String method = getMethod(request);
         return (HttpMethod.OPTIONS.matches(method) &&
                 request.getHeader(ORIGIN) != null &&
                 request.getHeader(ACCESS_CONTROL_REQUEST_METHOD) != null);
     }
 
-    public static String getContentType(NativeWebRequest request) {
+    public static String getContentType(@Nonnull NativeWebRequest request) {
         return request.getHeader(CONTENT_TYPE);
     }
 
     @Nullable
-    public static MediaType parseContentType(NativeWebRequest request) {
+    public static MediaType parseContentType(@Nonnull NativeWebRequest request) {
         MediaType mediaType = null;
         try {
             String contentTypeValue = getContentType(request);
@@ -95,7 +112,7 @@ public abstract class WebRequestUtils {
         return mediaType;
     }
 
-    public static boolean hasBody(NativeWebRequest request) {
+    public static boolean hasBody(@Nonnull NativeWebRequest request) {
         String contentLength = request.getHeader(CONTENT_LENGTH);
         String transferEncoding = request.getHeader(TRANSFER_ENCODING);
         return hasText(transferEncoding) ||
@@ -109,9 +126,104 @@ public abstract class WebRequestUtils {
      * @return the previously resolved lookupPath
      * @throws IllegalArgumentException if the lookup path is not found
      */
-    public static String getResolvedLookupPath(NativeWebRequest request) {
-        String lookupPath = (String) request.getAttribute(PATH_ATTRIBUTE, SCOPE_REQUEST);
+    public static String getResolvedLookupPath(@Nonnull NativeWebRequest request) {
+        String lookupPath = REQUEST.getAttribute(request, PATH_ATTRIBUTE);
         return lookupPath;
+    }
+
+    /**
+     * Get the best matching handler
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the best matching handler
+     * @throws IllegalArgumentException if the {@link NativeWebRequest} is null
+     * @see HandlerMethod
+     * @see org.springframework.web.servlet.HandlerMapping#getHandler(HttpServletRequest)
+     * @see org.springframework.web.reactive.HandlerMapping#getHandler(ServerWebExchange)
+     * @see org.springframework.web.servlet.HandlerMapping#BEST_MATCHING_HANDLER_ATTRIBUTE
+     * @see org.springframework.web.reactive.HandlerMapping#BEST_MATCHING_HANDLER_ATTRIBUTE
+     */
+    @Nullable
+    public static Object getBestMatchingHandler(@Nonnull NativeWebRequest request) {
+        SpringWebHelper springWebHelper = getSpringWebHelper(request);
+        return springWebHelper.getBestMatchingHandler(request);
+    }
+
+    /**
+     * Get the path within handler mapping
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the path within handler mapping
+     * @throws IllegalArgumentException if the {@link NativeWebRequest} is null
+     * @see org.springframework.web.servlet.HandlerMapping#PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
+     * @see org.springframework.web.reactive.HandlerMapping#PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
+     */
+    @Nullable
+    public static String getPathWithinHandlerMapping(@Nonnull NativeWebRequest request) {
+        SpringWebHelper springWebHelper = getSpringWebHelper(request);
+        return springWebHelper.getPathWithinHandlerMapping(request);
+    }
+
+    /**
+     * Get the best matching pattern
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the best matching pattern
+     * @throws IllegalArgumentException if the {@link NativeWebRequest} is null
+     * @see RequestMapping
+     * @see org.springframework.web.servlet.HandlerMapping#BEST_MATCHING_PATTERN_ATTRIBUTE
+     * @see org.springframework.web.reactive.HandlerMapping#BEST_MATCHING_PATTERN_ATTRIBUTE
+     */
+    @Nullable
+    public static String getBestMatchingPattern(@Nonnull NativeWebRequest request) {
+        SpringWebHelper springWebHelper = getSpringWebHelper(request);
+        return springWebHelper.getBestMatchingPattern(request);
+    }
+
+    /**
+     * Get the URI template variables
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the URI template variables
+     * @throws IllegalArgumentException if the {@link NativeWebRequest} is null
+     * @see RequestMapping#path()
+     * @see PathVariable
+     * @see org.springframework.web.servlet.HandlerMapping#URI_TEMPLATE_VARIABLES_ATTRIBUTE
+     * @see org.springframework.web.reactive.HandlerMapping#URI_TEMPLATE_VARIABLES_ATTRIBUTE
+     */
+    @Nullable
+    public static Map<String, String> getUriTemplateVariables(@Nonnull NativeWebRequest request) {
+        SpringWebHelper springWebHelper = getSpringWebHelper(request);
+        return springWebHelper.getUriTemplateVariables(request);
+    }
+
+    /**
+     * Get the producible media types
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the producible media types
+     * @throws IllegalArgumentException if the {@link NativeWebRequest} is null
+     * @see RequestMapping#produces()
+     * @see org.springframework.web.servlet.HandlerMapping#PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE
+     * @see org.springframework.web.reactive.HandlerMapping#PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE
+     */
+    @Nullable
+    public static Set<MediaType> getProducibleMediaTypes(@Nonnull NativeWebRequest request) {
+        SpringWebHelper springWebHelper = getSpringWebHelper(request);
+        return springWebHelper.getProducibleMediaTypes(request);
+    }
+
+    private static EnumMap<SpringWebType, SpringWebHelper> loadSpringWebHelpers() {
+        EnumMap<SpringWebType, SpringWebHelper> springWebHelpersMap = new EnumMap<>(SpringWebType.class);
+        ClassLoader classLoader = getClassLoader(SpringWebHelper.class);
+        List<SpringWebHelper> springWebHelpersList = loadFactories(SpringWebHelper.class, classLoader);
+        springWebHelpersList.forEach(helper -> springWebHelpersMap.put(helper.getType(), helper));
+        return springWebHelpersMap;
+    }
+
+    protected static SpringWebHelper getSpringWebHelper(NativeWebRequest request) {
+        SpringWebType springWebType = valueOf(request);
+        return springWebHelpers.getOrDefault(springWebType, INSTANCE);
     }
 
     private WebRequestUtils() {
