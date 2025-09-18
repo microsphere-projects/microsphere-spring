48d6f66d9f1686a08d17bbc9933e7451cbf95d52
Mercy <mercyblitz@gmail.com>
2025-09-18 20:16:38 +0800

    Update SpringWebHelper.java

--- microsphere-spring-web/src/main/java/io/microsphere/spring/web/util/SpringWebHelper.java
+++ microsphere-spring-web/src/main/java/io/microsphere/spring/web/util/SpringWebHelper.java
@@ -17,15 +17,18 @@
 
 package io.microsphere.spring.web.util;
 
+import io.microsphere.annotation.Nonnull;
+import io.microsphere.annotation.Nullable;
 import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.server.ServerWebExchange;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 import java.util.Set;
 
@@ -39,59 +42,128 @@ import java.util.Set;
 public interface SpringWebHelper {
 
     /**
-     * Get the best matching handler
+     * Get the HTTP method of the request
      *
      * @param request the {@link NativeWebRequest}
-     * @return the best matching handler
+     * @return the HTTP method if found, otherwise <code>null</code>
+     */
+    @Nullable
+    String getMethod(@Nonnull NativeWebRequest request);
+
+    /**
+     * Get the cookie value for the given cookie name.
+     *
+     * @param request    the {@link NativeWebRequest}
+     * @param cookieName the name of Cookie
+     * @return the cookie value if found, otherwise <code>null</code>
+     */
+    @Nullable
+    String getCookieValue(@Nonnull NativeWebRequest request, String cookieName);
+
+    /**
+     * Get the request body for the given request body type.
+     *
+     * @param request         the {@link NativeWebRequest}
+     * @param requestBodyType the request body type
+     * @param <T>             the type of request body
+     * @return the request body if found, otherwise <code>null</code>
+     * @see RequestBody
+     * @see org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor#resolveArgument
+     * @see org.springframework.web.reactive.result.method.annotation.RequestBodyMethodArgumentResolver#resolveArgument
+     */
+    <T> T getRequestBody(@Nonnull NativeWebRequest request, Class<T> requestBodyType);
+
+    /**
+     * Get the mapped handler for the best matching pattern.
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the mapped handler for the best matching pattern if found, otherwise <code>null</code>
      * @see HandlerMethod
-     * @see org.springframework.web.servlet.HandlerMapping#getHandler(HttpServletRequest)
-     * @see org.springframework.web.reactive.HandlerMapping#getHandler(ServerWebExchange)
+     * @see org.springframework.web.servlet.function.RouterFunction
+     * @see org.springframework.web.reactive.function.server.RouterFunction
+     * @see org.springframework.web.servlet.HandlerMapping#getHandler
+     * @see org.springframework.web.reactive.HandlerMapping#getHandler
      * @see org.springframework.web.servlet.HandlerMapping#BEST_MATCHING_HANDLER_ATTRIBUTE
      * @see org.springframework.web.reactive.HandlerMapping#BEST_MATCHING_HANDLER_ATTRIBUTE
      */
-    Object getBestMatchingHandler(NativeWebRequest request);
+    @Nullable
+    Object getBestMatchingHandler(@Nonnull NativeWebRequest request);
 
     /**
-     * Get the path within handler mapping
+     * Get the path within the handler mapping, in case of a pattern match, or the full relevant URI
+     * (typically within the DispatcherServlet's mapping) else.
      *
      * @param request the {@link NativeWebRequest}
-     * @return the path within handler mapping
+     * @return the path within handler mapping if found, otherwise <code>null</code>
+     * @see org.springframework.web.servlet.handler.AbstractUrlHandlerMapping#exposePathWithinMapping
+     * @see org.springframework.web.reactive.handler.AbstractUrlHandlerMapping#lookupHandler
      * @see org.springframework.web.servlet.HandlerMapping#PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
      * @see org.springframework.web.reactive.HandlerMapping#PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE
      */
-    String getPathWithinHandlerMapping(NativeWebRequest request);
+    @Nullable
+    String getPathWithinHandlerMapping(@Nonnull NativeWebRequest request);
 
     /**
-     * Get the best matching pattern
+     * Get the best matching pattern within the handler mapping.
      *
      * @param request the {@link NativeWebRequest}
-     * @return the best matching pattern
-     * @see RequestMapping
+     * @return the best matching pattern within the handler mapping if found, otherwise <code>null</code>
+     * @see RequestMapping#path()
+     * @see org.springframework.web.servlet.handler.AbstractUrlHandlerMapping#exposePathWithinMapping
+     * @see org.springframework.web.reactive.handler.AbstractUrlHandlerMapping#lookupHandler
      * @see org.springframework.web.servlet.HandlerMapping#BEST_MATCHING_PATTERN_ATTRIBUTE
      * @see org.springframework.web.reactive.HandlerMapping#BEST_MATCHING_PATTERN_ATTRIBUTE
      */
-    String getBestMatchingPattern(NativeWebRequest request);
+    @Nullable
+    String getBestMatchingPattern(@Nonnull NativeWebRequest request);
 
     /**
-     * Get the URI template variables
+     * Get the URI templates map, mapping variable names to values.
      *
      * @param request the {@link NativeWebRequest}
-     * @return the URI template variables
-     * @see RequestMapping#path()
+     * @return the URI template variables if found, otherwise <code>null</code>
      * @see PathVariable
+     * @see org.springframework.web.servlet.handler.AbstractUrlHandlerMapping#exposeUriTemplateVariables
+     * @see org.springframework.web.reactive.handler.AbstractUrlHandlerMapping#lookupHandler
      * @see org.springframework.web.servlet.HandlerMapping#URI_TEMPLATE_VARIABLES_ATTRIBUTE
      * @see org.springframework.web.reactive.HandlerMapping#URI_TEMPLATE_VARIABLES_ATTRIBUTE
      */
-    Map<String, String> getUriTemplateVariables(NativeWebRequest request);
+    @Nullable
+    Map<String, String> getUriTemplateVariables(@Nonnull NativeWebRequest request);
 
     /**
-     * Get the producible media types
+     * Get a map with URI variable names and a corresponding {@link MultiValueMap} of URI matrix variables for each.
      *
      * @param request the {@link NativeWebRequest}
-     * @return the producible media types
+     * @return the matrix variables if found, otherwise <code>null</code>
+     * @see MatrixVariable
+     * @see org.springframework.web.servlet.mvc.method.annotation.MatrixVariableMapMethodArgumentResolver#resolveArgument
+     * @see org.springframework.web.reactive.result.method.annotation.MatrixVariableMapMethodArgumentResolver#resolveArgumentValue
+     * @see org.springframework.web.servlet.HandlerMapping#MATRIX_VARIABLES_ATTRIBUTE
+     * @see org.springframework.web.reactive.HandlerMapping#MATRIX_VARIABLES_ATTRIBUTE
+     */
+    @Nullable
+    Map<String, MultiValueMap<String, String>> getMatrixVariables(@Nonnull NativeWebRequest request);
+
+    /**
+     * Get the set of producible MediaTypes applicable to the mapped handler.
+     *
+     * @param request the {@link NativeWebRequest}
+     * @return the producible media types if found, otherwise <code>null</code>
      * @see RequestMapping#produces()
+     * @see org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping#handleMatch
+     * @see org.springframework.web.reactive.result.method.RequestMappingInfoHandlerMapping#handleMatch
      * @see org.springframework.web.servlet.HandlerMapping#PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE
      * @see org.springframework.web.reactive.HandlerMapping#PRODUCIBLE_MEDIA_TYPES_ATTRIBUTE
      */
-    Set<MediaType> getProducibleMediaTypes(NativeWebRequest request);
+    @Nullable
+    Set<MediaType> getProducibleMediaTypes(@Nonnull NativeWebRequest request);
+
+    /**
+     * Get the Spring Web Type
+     *
+     * @return {@link SpringWebType}
+     */
+    @Nonnull
+    SpringWebType getType();
 }
