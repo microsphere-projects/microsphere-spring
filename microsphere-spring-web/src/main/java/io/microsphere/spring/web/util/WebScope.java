4584a9e92074b9e860236a211c1ffca7865d5c04
Mercy <mercyblitz@gmail.com>
2025-09-18 20:16:51 +0800

    Update WebScope.java

--- microsphere-spring-web/src/main/java/io/microsphere/spring/web/util/WebScope.java
+++ microsphere-spring-web/src/main/java/io/microsphere/spring/web/util/WebScope.java
@@ -18,7 +18,7 @@
 package io.microsphere.spring.web.util;
 
 import io.microsphere.annotation.Nonnull;
-import org.springframework.lang.Nullable;
+import io.microsphere.annotation.Nullable;
 import org.springframework.web.context.request.RequestAttributes;
 
 import java.util.Map;
