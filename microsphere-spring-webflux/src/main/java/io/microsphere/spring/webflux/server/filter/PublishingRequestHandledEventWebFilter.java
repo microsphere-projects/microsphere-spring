d0d6ba747b882516d8db27b67efba21dc2f5ece3
Mercy Ma <mercyblitz@gmail.com>
2025-09-23 13:42:40 +0800

    Create PublishingRequestHandledEventWebFilter.java

---
+++ microsphere-spring-webflux/src/main/java/io/microsphere/spring/webflux/server/filter/PublishingRequestHandledEventWebFilter.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.webflux.server.filter;
+
+import io.microsphere.spring.webflux.context.event.ServerRequestHandledEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.core.Ordered;
+import org.springframework.web.context.support.RequestHandledEvent;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.server.WebHandler;
+import org.springframework.web.server.handler.DefaultWebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * A WebFilter class to publish the {@link RequestHandledEvent}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see WebFilter
+ * @see RequestHandledEvent
+ * @since 1.0.0
+ */
+public class PublishingRequestHandledEventWebFilter implements WebFilter, ApplicationEventPublisherAware, Ordered {
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        long startTime = System.currentTimeMillis();
+        publishEvent(exchange, chain, startTime);
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    private void publishEvent(ServerWebExchange exchange, WebFilterChain chain, long startTime) {
+        RequestHandledEvent event = createEvent(exchange, chain, startTime);
+        this.applicationEventPublisher.publishEvent(event);
+    }
+
+    private RequestHandledEvent createEvent(ServerWebExchange exchange, WebFilterChain chain, long startTime) {
+        DefaultWebFilterChain filterChain = (DefaultWebFilterChain) chain;
+        WebHandler webHandler = filterChain.getHandler();
+        long processingTimeMillis = System.currentTimeMillis() - startTime;
+        return new ServerRequestHandledEvent(webHandler, exchange, processingTimeMillis);
+    }
+
+    @Override
+    public int getOrder() {
+        return LOWEST_PRECEDENCE;
+    }
+}
