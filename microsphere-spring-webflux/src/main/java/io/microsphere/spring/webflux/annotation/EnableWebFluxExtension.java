7eda63d07a0a6374192a9811166b9a988f73daf2
Mercy Ma <mercyblitz@gmail.com>
2025-09-28 11:54:52 +0800

    Update EnableWebFluxExtension.java

--- microsphere-spring-webflux/src/main/java/io/microsphere/spring/webflux/annotation/EnableWebFluxExtension.java
+++ microsphere-spring-webflux/src/main/java/io/microsphere/spring/webflux/annotation/EnableWebFluxExtension.java
@@ -21,12 +21,17 @@ import io.microsphere.spring.web.event.HandlerMethodArgumentsResolvedEvent;
 import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
 import io.microsphere.spring.web.event.WebEventPublisher;
 import io.microsphere.spring.web.metadata.WebEndpointMapping;
+import io.microsphere.spring.web.metadata.WebEndpointMappingFactory;
+import io.microsphere.spring.web.metadata.WebEndpointMappingFilter;
 import io.microsphere.spring.web.metadata.WebEndpointMappingRegistry;
+import io.microsphere.spring.web.metadata.WebEndpointMappingResolver;
 import io.microsphere.spring.web.method.support.HandlerMethodArgumentInterceptor;
 import io.microsphere.spring.web.method.support.HandlerMethodInterceptor;
 import io.microsphere.spring.web.util.RequestAttributesUtils;
+import io.microsphere.spring.web.util.RequestContextStrategy;
 import io.microsphere.spring.webflux.context.event.ServerRequestHandledEvent;
 import io.microsphere.spring.webflux.handler.ReversedProxyHandlerMapping;
+import io.microsphere.spring.webflux.server.filter.RequestContextWebFilter;
 import io.microsphere.spring.webflux.server.filter.RequestHandledEventPublishingWebFilter;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.MethodParameter;
@@ -34,6 +39,7 @@ import org.springframework.core.annotation.AliasFor;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.support.RequestHandledEvent;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.server.ServerWebExchange;
@@ -43,6 +49,7 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static io.microsphere.spring.web.util.RequestContextStrategy.DEFAULT;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -68,7 +75,10 @@ public @interface EnableWebFluxExtension {
      *
      * @return <code>true</code> as default
      * @see WebEndpointMapping
+     * @see WebEndpointMappingResolver
      * @see WebEndpointMappingRegistry
+     * @see WebEndpointMappingFactory
+     * @see WebEndpointMappingFilter
      */
     @AliasFor(annotation = EnableWebExtension.class)
     boolean registerWebEndpointMappings() default true;
@@ -103,6 +113,18 @@ public @interface EnableWebFluxExtension {
      */
     @AliasFor(annotation = EnableWebExtension.class)
     boolean publishEvents() default true;
+
+    /**
+     * Indicate where the {@link RequestAttributes} stores.
+     *
+     * @return {@link RequestContextStrategy#DEFAULT} as default
+     * @see RequestAttributes
+     * @see RequestContextHolder
+     * @see RequestContextWebFilter
+     * @see RequestContextStrategy
+     */
+    @AliasFor(annotation = EnableWebExtension.class)
+    RequestContextStrategy requestContextStrategy() default DEFAULT;
 
     /**
      * Indicate that {@link RequestAttributes} stores the {@link MethodParameter argument} of {@link HandlerMethod} that annotated {@link RequestBody}
