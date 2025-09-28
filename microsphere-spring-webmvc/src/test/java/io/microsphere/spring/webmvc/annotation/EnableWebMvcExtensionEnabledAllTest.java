e6d5a3e35c8aac82c6ed01662b2468855cbd64bc
Mercy Ma <mercyblitz@gmail.com>
2025-09-28 14:20:31 +0800

    Update EnableWebMvcExtensionEnabledAllTest.java

--- microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtensionEnabledAllTest.java
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtensionEnabledAllTest.java
@@ -18,10 +18,34 @@
 package io.microsphere.spring.webmvc.annotation;
 
 
+import io.microsphere.spring.web.event.HandlerMethodArgumentsResolvedEvent;
+import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
+import io.microsphere.spring.web.metadata.WebEndpointMapping;
+import io.microsphere.spring.web.method.support.HandlerMethodArgumentInterceptor;
+import io.microsphere.spring.web.method.support.HandlerMethodInterceptor;
+import io.microsphere.spring.webmvc.handler.ReversedProxyHandlerMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.function.RouterFunction;
 
 /**
- * {@link EnableWebMvcExtension} Test when all status are enabled(all attributes are <code>true</code>).
+ * {@link EnableWebMvcExtension} Test when all status are enabled with test cases:
+ * <ul>
+ *     <li>All {@link WebEndpointMapping WebEndpointMappings} will be exposed from {@link Controller @Controller} and 
+ *     {@link RouterFunction}</li>
+ *     <li>All {@link HandlerMethodArgumentInterceptor} or {@link HandlerMethodInterceptor} beans will be registered and
+ *     intercept the {@link HandlerMethod HandlerMethods} of {@link Controller @Controller}</li>
+ *     <li>The {@link WebEndpointMappingsReadyEvent} and {@link HandlerMethodArgumentsResolvedEvent} will be published</li>
+ *     <li>All {@link HandlerInterceptor} beans will be registered into {@link InterceptorRegistry}</li>
+ *     <li>The {@link RequestBody} method arguments of {@link Controller @Controller} will be stored</li>
+ *     <li>The {@link ResponseBody} method return values of {@link Controller @Controller} will be stored</li>
+ *     <li>The {@link ReversedProxyHandlerMapping} bean will be registered</li>
+ * </ul>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see WebMvcExtensionBeanDefinitionRegistrar
