b395501adc01fc6ce37607a4a99f923f75dd5ade
Mercy Ma <mercyblitz@gmail.com>
2025-09-28 14:47:43 +0800

    Update Javadoc to use generic Controller references
    Replaced specific references to TestController and RouterFunctionTestConfig in Javadoc with generic references to @Controller and RouterFunction. This improves clarity and generalizes the documentation for broader applicability.

--- microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/annotation/EnableWebFluxExtensionAllDisabledTest.java
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/annotation/EnableWebFluxExtensionAllDisabledTest.java
@@ -17,32 +17,32 @@
 
 package io.microsphere.spring.webflux.annotation;
 
-import io.microsphere.spring.test.web.controller.TestController;
 import io.microsphere.spring.web.event.HandlerMethodArgumentsResolvedEvent;
 import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
 import io.microsphere.spring.web.metadata.WebEndpointMapping;
 import io.microsphere.spring.webflux.context.event.ServerRequestHandledEvent;
 import io.microsphere.spring.webflux.handler.ReversedProxyHandlerMapping;
 import io.microsphere.spring.webflux.server.filter.RequestContextWebFilter;
-import io.microsphere.spring.webflux.test.RouterFunctionTestConfig;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.reactive.function.server.RouterFunction;
 
 import static io.microsphere.spring.web.util.RequestContextStrategy.DEFAULT;
 
 /**
  * {@link EnableWebFluxExtension} Test when all status are disabled with test cases:
  * <ul>
- *     <li>No {@link WebEndpointMapping} will be exposed from {@link TestController} and {@link RouterFunctionTestConfig}</li>
- *     <li>All {@link HandlerMethod HandlerMethods} of {@link TestController} will not be intercepted</li>
+ *     <li>No {@link WebEndpointMapping} will be exposed from from {@link Controller @Controller} and {@link RouterFunction}</li>
+ *     <li>No {@link HandlerMethod HandlerMethods} of {@link Controller @Controller} will be intercepted</li>
  *     <li>No ApplicationEvent {@link WebEndpointMappingsReadyEvent} or {@link HandlerMethodArgumentsResolvedEvent}
  *     or {@link ServerRequestHandledEvent} will be published</li>
  *     <li>No {@link RequestContextWebFilter} bean will be registered</li>
- *     <li>All {@link RequestBody} method arguments of {@link TestController} will not be stored</li>
- *     <li>All {@link ResponseBody} method return values of {@link TestController} will not be stored</li>
- *     <li>The {@link ReversedProxyHandlerMapping} bean will not be registered</li>
+ *     <li>No {@link RequestBody} method arguments of {@link Controller @Controller} will be stored</li>
+ *     <li>No {@link ResponseBody} method return values of {@link Controller @Controller} will be stored</li>
+ *     <li>No {@link ReversedProxyHandlerMapping} bean will be registered</li>
  * </ul>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
