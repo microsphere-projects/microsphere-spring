27ae3bfeec7625eee2f95763e1875b3ccc8a1180
Mercy Ma <mercyblitz@gmail.com>
2025-09-28 13:33:47 +0800

    Enhance WebFlux extension test documentation and config
    Expanded the class-level Javadoc to detail all test cases covered, including event publication, bean registration, and method argument/return value storage. Updated @EnableWebFluxExtension annotation to specify THREAD_LOCAL request context strategy.

--- microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/annotation/EnableWebFluxExtensionInterceptorsTest.java
+++ microsphere-spring-webflux/src/test/java/io/microsphere/spring/webflux/annotation/EnableWebFluxExtensionInterceptorsTest.java
@@ -17,13 +17,40 @@
 
 package io.microsphere.spring.webflux.annotation;
 
+import io.microsphere.spring.test.web.controller.TestController;
+import io.microsphere.spring.web.event.HandlerMethodArgumentsResolvedEvent;
+import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
+import io.microsphere.spring.web.metadata.WebEndpointMapping;
 import io.microsphere.spring.web.method.support.HandlerMethodArgumentInterceptor;
 import io.microsphere.spring.web.method.support.HandlerMethodInterceptor;
+import io.microsphere.spring.webflux.context.event.ServerRequestHandledEvent;
+import io.microsphere.spring.webflux.handler.ReversedProxyHandlerMapping;
+import io.microsphere.spring.webflux.server.filter.RequestContextWebFilter;
 import io.microsphere.spring.webflux.test.EnableWebFluxExtensionInterceptorsTestConfig;
+import io.microsphere.spring.webflux.test.RouterFunctionTestConfig;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.HandlerMethod;
+
+import static io.microsphere.spring.web.util.RequestContextStrategy.THREAD_LOCAL;
 
 /**
- * {@link EnableWebFluxExtension} Test with {@link HandlerMethodInterceptor} and {@link HandlerMethodArgumentInterceptor}.
+ * {@link EnableWebFluxExtension} Test with test cases :
+ * <ul>
+ *     <li>All {@link WebEndpointMapping WebEndpointMappings} will be exposed from {@link TestController} and
+ *     {@link RouterFunctionTestConfig}</li>
+ *     <li>The {@link EnableWebFluxExtensionInterceptorsTestConfig}({@link HandlerMethodArgumentInterceptor}
+ *     and {@link HandlerMethodInterceptor}) bean will be registered and intercept the
+ *     {@link HandlerMethod HandlerMethods} of {@link Controller Controllers}</li>
+ *     <li>The {@link WebEndpointMappingsReadyEvent}, {@link HandlerMethodArgumentsResolvedEvent}
+ *     and {@link ServerRequestHandledEvent} will be published</li>
+ *     <li>The {@link RequestContextWebFilter} with {@link ThreadLocal} bean will be registered</li>
+ *     <li>The {@link RequestBody} method arguments of {@link Controller Controllers} will be stored</li>
+ *     <li>The {@link ResponseBody} method return values of {@link Controller Controllers} will be stored</li>
+ *     <li>The {@link ReversedProxyHandlerMapping} bean will not be registered</li>
+ * </ul>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see EnableWebFluxExtension
@@ -33,6 +60,6 @@ import org.springframework.test.context.ContextConfiguration;
         EnableWebFluxExtensionInterceptorsTest.class,
         EnableWebFluxExtensionInterceptorsTestConfig.class
 })
-@EnableWebFluxExtension
+@EnableWebFluxExtension(requestContextStrategy = THREAD_LOCAL)
 class EnableWebFluxExtensionInterceptorsTest extends AbstractEnableWebFluxExtensionTest {
 }
