52c1250016d3cc66db02d8900bc9e75345aefa1d
Mercy Ma <mercyblitz@gmail.com>
2025-09-28 14:09:20 +0800

    Update EnableWebMvcExtensionAllDisabledTest.java

--- microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtensionAllDisabledTest.java
+++ microsphere-spring-webmvc/src/test/java/io/microsphere/spring/webmvc/annotation/EnableWebMvcExtensionAllDisabledTest.java
@@ -16,10 +16,31 @@
  */
 package io.microsphere.spring.webmvc.annotation;
 
+import io.microsphere.spring.test.web.controller.TestController;
+import io.microsphere.spring.web.event.HandlerMethodArgumentsResolvedEvent;
+import io.microsphere.spring.web.event.WebEndpointMappingsReadyEvent;
+import io.microsphere.spring.web.metadata.WebEndpointMapping;
+import io.microsphere.spring.webmvc.handler.ReversedProxyHandlerMapping;
+import io.microsphere.spring.webmvc.test.RouterFunctionTestConfig;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 /**
- * {@link EnableWebMvcExtension} Test when all status are disabled(all attributes are <code>false</code>).
+ * {@link EnableWebMvcExtension} Test when all status are disabled with test cases:
+ * <ul>
+ *     <li>No {@link WebEndpointMapping} will be exposed from {@link TestController} and {@link RouterFunctionTestConfig}</li>
+ *     <li>All {@link HandlerMethod HandlerMethods} of {@link TestController} will not be intercepted</li>
+ *     <li>No ApplicationEvent {@link WebEndpointMappingsReadyEvent} or {@link HandlerMethodArgumentsResolvedEvent}
+ *     will be published</li>
+ *     <li>No {@link HandlerInterceptor} bean will be registered into {@link InterceptorRegistry}</li>
+ *     <li>All {@link RequestBody} method arguments of {@link TestController} will not be stored</li>
+ *     <li>All {@link ResponseBody} method return values of {@link TestController} will not be stored</li>
+ *     <li>The {@link ReversedProxyHandlerMapping} bean will not be registered</li>
+ * </ul>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @see EnableWebMvcExtension
