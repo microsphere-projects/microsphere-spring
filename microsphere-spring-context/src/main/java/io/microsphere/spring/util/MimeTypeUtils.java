941230ce7c703f97d733711da773b012e082d320
Mercy Ma <mercyblitz@gmail.com>
2025-11-06 19:16:36 +0800

    Add SpecificityComparator for MimeType sorting
    Introduced a public static inner class SpecificityComparator to allow sorting of MimeType instances by specificity, considering wildcard types, subtypes, and parameter count. This facilitates more precise handling and ordering of MIME types in collections.

--- microsphere-spring-context/src/main/java/io/microsphere/spring/util/MimeTypeUtils.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/util/MimeTypeUtils.java
@@ -21,6 +21,7 @@ import io.microsphere.util.Utils;
 import org.springframework.util.MimeType;
 
 import java.util.Collection;
+import java.util.Comparator;
 
 import static io.microsphere.collection.CollectionUtils.isEmpty;
 
@@ -108,7 +109,39 @@ public abstract class MimeTypeUtils implements Utils {
         return null;
     }
 
-    private MimeTypeUtils() {
+    /**
+     * A {@link Comparator} for {@link MimeType} that orders by specificity.
+     */
+    public static class SpecificityComparator<T extends MimeType> implements Comparator<T> {
+
+        @Override
+        public int compare(T mimeType1, T mimeType2) {
+            if (mimeType1.isWildcardType() && !mimeType2.isWildcardType()) {  // */* < audio/*
+                return 1;
+            } else if (mimeType2.isWildcardType() && !mimeType1.isWildcardType()) {  // audio/* > */*
+                return -1;
+            } else if (!mimeType1.getType().equals(mimeType2.getType())) {  // audio/basic == text/html
+                return 0;
+            } else {  // mediaType1.getType().equals(mediaType2.getType())
+                if (mimeType1.isWildcardSubtype() && !mimeType2.isWildcardSubtype()) {  // audio/* < audio/basic
+                    return 1;
+                } else if (mimeType2.isWildcardSubtype() && !mimeType1.isWildcardSubtype()) {  // audio/basic > audio/*
+                    return -1;
+                } else if (!mimeType1.getSubtype().equals(mimeType2.getSubtype())) {  // audio/basic == audio/wave
+                    return 0;
+                } else {  // mediaType2.getSubtype().equals(mediaType2.getSubtype())
+                    return compareParameters(mimeType1, mimeType2);
+                }
+            }
+        }
+
+        protected int compareParameters(T mimeType1, T mimeType2) {
+            int paramsSize1 = mimeType1.getParameters().size();
+            int paramsSize2 = mimeType2.getParameters().size();
+            return Integer.compare(paramsSize2, paramsSize1);  // audio/basic;level=1 < audio/basic
+        }
     }
 
+    private MimeTypeUtils() {
+    }
 }
