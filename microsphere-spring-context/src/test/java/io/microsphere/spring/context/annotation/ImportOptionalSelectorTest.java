0f7fb65b5fb75f3d4f3d0b7c54398895da92a05e
Mercy <mercyblitz@gmail.com>
2026-03-06 11:06:11 +0800

    Add @ImportOptional annotation and selector #152
    Introduce @ImportOptional to allow importing component classes by name even if some are absent on the classpath. Add ImportOptionalSelector (an ImportSelector) that resolves placeholder attributes, attempts to load each configured class via the classloader, filters out missing classes, and returns only resolvable class names. Include unit tests (ImportOptionalSelectorTest) to verify behavior when target classes are present and absent.

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/annotation/ImportOptionalSelectorTest.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.context.annotation;
+
+
+import io.microsphere.spring.test.domain.User;
+import org.junit.Test;
+
+import static io.microsphere.spring.beans.BeanUtils.isBeanPresent;
+import static io.microsphere.spring.test.util.SpringTestUtils.testInSpringContainer;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * {@link ImportOptionalSelector} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ImportOptionalSelector
+ * @see ImportOptional
+ * @since 1.0.0
+ */
+public class ImportOptionalSelectorTest {
+
+    @Test
+    public void testPresent() {
+        testInSpringContainer(context -> {
+            assertNotNull(context.getBean(User.class));
+        }, PresentConfig.class);
+    }
+
+    @Test
+    public void testAbsent() {
+        testInSpringContainer(context -> {
+            assertFalse(isBeanPresent(context, "notFound"));
+            assertFalse(isBeanPresent(context, "NotFound"));
+            assertFalse(isBeanPresent(context, "io.microsphere.NotFound"));
+        }, AbsentConfig.class);
+    }
+
+    @ImportOptional(value = {
+            "io.microsphere.spring.test.domain.User"
+    })
+    static class PresentConfig {
+    }
+
+    @ImportOptional(value = {
+            "io.microsphere.NotFound"
+    })
+    static class AbsentConfig {
+    }
+}
