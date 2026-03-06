d5ae1950d8296e1b9563d50c2f313d35c3e79fdc
Mercy Ma <mercyblitz@gmail.com>
2026-03-06 19:47:39 +0800

    Refactor registry utils; add scanner helpers & tests
    Refactor AnnotatedBeanDefinitionRegistryUtils: replace pattern-matching casts with explicit casts, use collection/array utilities (newArrayList, arrayToString, newLinkedHashSet), improve logging messages, and streamline register/scan flows. Add a new findBeanDefinitionHolders overload that resolves a BeanNameGenerator from the scanner, and simplify resolveAnnotatedBeanNameGenerator logging. Update tests to use a beanFactory field, rename usages, and add tests for bean-name generator resolution, isPresentBean, and findBeanDefinitionHolders (including registration). Also add spring-context-indexer as an optional test dependency in microsphere-spring-test/pom.xml.

--- microsphere-spring-context/src/test/java/io/microsphere/spring/context/annotation/AnnotatedBeanDefinitionRegistryUtilsTest.java
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/context/annotation/AnnotatedBeanDefinitionRegistryUtilsTest.java
@@ -2,14 +2,31 @@ package io.microsphere.spring.context.annotation;
 
 import io.microsphere.spring.beans.test.TestBean;
 import io.microsphere.spring.beans.test.TestBean2;
+import io.microsphere.spring.test.domain.User;
+import io.microsphere.spring.test.web.controller.TestController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 
+import java.util.Set;
+
+import static io.microsphere.spring.context.annotation.AnnotatedBeanDefinitionRegistryUtils.findBeanDefinitionHolders;
+import static io.microsphere.spring.context.annotation.AnnotatedBeanDefinitionRegistryUtils.isPresentBean;
 import static io.microsphere.spring.context.annotation.AnnotatedBeanDefinitionRegistryUtils.registerBeans;
+import static io.microsphere.spring.context.annotation.AnnotatedBeanDefinitionRegistryUtils.resolveAnnotatedBeanNameGenerator;
 import static io.microsphere.spring.context.annotation.AnnotatedBeanDefinitionRegistryUtils.scanBasePackages;
+import static io.microsphere.spring.test.util.SpringTestUtils.testInSpringContainer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.beans.factory.support.BeanDefinitionReaderUtils.registerBeanDefinition;
 import static org.springframework.context.annotation.AnnotationConfigUtils.registerAnnotationConfigProcessors;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
@@ -22,52 +39,90 @@ import static org.springframework.util.ObjectUtils.isEmpty;
  */
 class AnnotatedBeanDefinitionRegistryUtilsTest {
 
-    private DefaultListableBeanFactory registry = null;
+    private DefaultListableBeanFactory beanFactory = null;
 
     @BeforeEach
     void setUp() {
-        registry = new DefaultListableBeanFactory();
-        registry.setAllowBeanDefinitionOverriding(false);
-        registerAnnotationConfigProcessors(registry);
+        this.beanFactory = new DefaultListableBeanFactory();
+        this.beanFactory.setAllowBeanDefinitionOverriding(false);
+        registerAnnotationConfigProcessors(this.beanFactory);
     }
 
     @Test
     void testRegisterBeans() {
 
         for (int i = 0; i < 100; i++) {
-            registerBeans(registry, this.getClass());
+            registerBeans(this.beanFactory, this.getClass());
         }
 
-        String[] beanNames = registry.getBeanNamesForType(this.getClass());
+        String[] beanNames = this.beanFactory.getBeanNamesForType(this.getClass());
 
         assertEquals(1, beanNames.length);
 
-        beanNames = registry.getBeanNamesForType(AnnotatedBeanDefinitionRegistryUtils.class);
+        beanNames = this.beanFactory.getBeanNamesForType(AnnotatedBeanDefinitionRegistryUtils.class);
 
         assertTrue(isEmpty(beanNames));
 
-        registerBeans(registry);
+        registerBeans(this.beanFactory);
 
     }
 
     @Test
     void testScanBasePackages() {
 
-        int count = scanBasePackages(registry, TestBean.class.getPackage().getName());
+        int count = scanBasePackages(this.beanFactory, TestBean.class.getPackage().getName());
 
         assertEquals(2, count);
 
-        String[] beanNames = registry.getBeanNamesForType(TestBean.class);
+        String[] beanNames = this.beanFactory.getBeanNamesForType(TestBean.class);
 
         assertEquals(1, beanNames.length);
 
-        beanNames = registry.getBeanNamesForType(TestBean2.class);
+        beanNames = this.beanFactory.getBeanNamesForType(TestBean2.class);
 
         assertEquals(1, beanNames.length);
 
-        count = scanBasePackages(registry);
+        count = scanBasePackages(this.beanFactory);
 
         assertEquals(0, count);
     }
 
+
+    @Test
+    void testResolveAnnotatedBeanNameGenerator() {
+        testInSpringContainer(context -> {
+            AnnotationConfigApplicationContext ctx = (AnnotationConfigApplicationContext) context;
+            AnnotationBeanNameGenerator annotationBeanNameGenerator = new AnnotationBeanNameGenerator();
+            ctx.setBeanNameGenerator(annotationBeanNameGenerator);
+            DefaultListableBeanFactory beanFactory = (DefaultListableBeanFactory) context.getBeanFactory();
+            BeanNameGenerator beanNameGenerator = resolveAnnotatedBeanNameGenerator(beanFactory);
+            assertSame(annotationBeanNameGenerator, beanNameGenerator);
+        });
+    }
+
+    @Test
+    void testIsPresentBean() {
+        assertFalse(isPresentBean(this.beanFactory, TestController.class));
+    }
+
+    @Test
+    void testResolveAnnotatedBeanNameGeneratorWithNull() {
+        BeanNameGenerator beanNameGenerator = resolveAnnotatedBeanNameGenerator(null);
+        assertNotNull(beanNameGenerator);
+
+        assertEquals(beanNameGenerator.getClass(), resolveAnnotatedBeanNameGenerator(this.beanFactory).getClass());
+    }
+
+    @Test
+    void testFindBeanDefinitionHolders() {
+        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(this.beanFactory);
+        String packageName = TestController.class.getPackage().getName();
+        Set<BeanDefinitionHolder> beanDefinitionHolders = findBeanDefinitionHolders(scanner, packageName);
+        assertEquals(1, beanDefinitionHolders.size());
+        assertFalse(isPresentBean(this.beanFactory, TestController.class));
+
+        beanDefinitionHolders.forEach(h -> registerBeanDefinition(h, this.beanFactory));
+        assertTrue(isPresentBean(this.beanFactory, TestController.class));
+        assertFalse(isPresentBean(this.beanFactory, User.class));
+    }
 }
