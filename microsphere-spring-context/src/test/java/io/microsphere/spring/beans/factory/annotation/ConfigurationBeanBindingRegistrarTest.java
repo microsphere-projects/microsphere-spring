06f806ee4af626a3d20d63cb9ca15dafd61c5377
Mercy Ma <mercyblitz@gmail.com>
2026-03-07 11:40:57 +0800

    Refactor config bean binding registrar and tests
    Refactor ConfigurationBeanBindingRegistrar to use shared utilities and simplify registration logic: replace manual LinkedHashSet with newLinkedHashSet, use DOT_CHAR for property splitting, consistently reference this.environment, and use registerBeanDefinition/registerInfrastructureBean helpers with the BEAN_NAME constant (removing direct logging). Add ConfigurationBeanBindingRegistrarTest to cover single and multiple configuration bean registration scenarios using MockEnvironment and AnnotationConfigApplicationContext.

---
+++ microsphere-spring-context/src/test/java/io/microsphere/spring/beans/factory/annotation/ConfigurationBeanBindingRegistrarTest.java
@@ -0,0 +1,131 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.beans.factory.annotation;
+
+
+import io.microsphere.spring.test.domain.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.mock.env.MockEnvironment;
+
+import java.util.Map;
+
+import static io.microsphere.spring.beans.factory.annotation.EnableConfigurationBeanBinding.DEFAULT_IGNORE_INVALID_FIELDS;
+import static io.microsphere.spring.beans.factory.annotation.EnableConfigurationBeanBinding.DEFAULT_IGNORE_UNKNOWN_FIELDS;
+import static io.microsphere.spring.beans.factory.annotation.EnableConfigurationBeanBinding.DEFAULT_MULTIPLE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * {@link ConfigurationBeanBindingRegistrar} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ConfigurationBeanBindingRegistrar
+ * @since 1.0.0
+ */
+class ConfigurationBeanBindingRegistrarTest {
+
+    private AnnotationConfigApplicationContext context;
+
+    private DefaultListableBeanFactory beanFactory;
+
+    private MockEnvironment environment;
+
+    private ConfigurationBeanBindingRegistrar registrar;
+
+    private AnnotationAttributes attributes;
+
+    @BeforeEach
+    void setUp() {
+        this.context = new AnnotationConfigApplicationContext();
+        this.beanFactory = (DefaultListableBeanFactory) this.context.getBeanFactory();
+        this.environment = new MockEnvironment();
+        this.registrar = new ConfigurationBeanBindingRegistrar();
+        this.registrar.setBeanFactory(this.beanFactory);
+        this.registrar.setEnvironment(this.environment);
+        this.attributes = new AnnotationAttributes();
+
+        this.attributes.put("prefix", "user");
+        this.attributes.put("type", User.class);
+        this.attributes.put("multiple", DEFAULT_MULTIPLE);
+        this.attributes.put("ignoreUnknownFields", DEFAULT_IGNORE_UNKNOWN_FIELDS);
+        this.attributes.put("ignoreInvalidFields", DEFAULT_IGNORE_INVALID_FIELDS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.context.close();
+    }
+
+    @Test
+    void testOnDefaults() {
+        assertSingle(null, "Mercy", 18);
+    }
+
+    @Test
+    void testOnSingle() {
+        assertSingle("u0", "Ma", 28);
+    }
+
+    @Test
+    void testOnMultiple() {
+        this.attributes.put("prefix", "users");
+        this.attributes.put("multiple", true);
+
+        this.environment.setProperty("users.u0", "-");
+        this.environment.setProperty("users.u1.name", "Mercy");
+        this.environment.setProperty("users.u1.age", "18");
+
+        this.environment.setProperty("users.u2.name", "Ma");
+        this.environment.setProperty("users.u2.age", "28");
+
+        this.registrar.registerConfigurationBeanDefinitions(this.attributes, this.beanFactory);
+
+        this.context.refresh();
+
+        assertUserBean("u1", "Mercy", 18);
+        assertUserBean("u2", "Ma", 28);
+    }
+
+    void assertSingle(String beanName, String name, int age) {
+        if (beanName != null) {
+            this.environment.setProperty("user.id", beanName);
+        }
+        this.environment.setProperty("user.name", name);
+        this.environment.setProperty("user.age", age);
+
+        this.registrar.registerConfigurationBeanDefinitions(this.attributes, this.beanFactory);
+
+        this.context.refresh();
+
+        assertUserBean(beanName, name, age);
+    }
+
+    private void assertUserBean(String id, String name, int age) {
+        Map<String, User> beansMap = this.context.getBeansOfType(User.class);
+        String beanName = id == null ? User.class.getName() + "#0" : id;
+        User user = beansMap.get(beanName);
+        assertNotNull(user);
+        assertEquals(name, user.getName());
+        assertEquals(age, user.getAge());
+    }
+}
