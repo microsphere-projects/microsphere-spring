0607416ba51e05ea179e5228a726dfc942ba5be9
Mercy <mercyblitz@gmail.com>
2026-03-09 19:08:05 +0800

    Use BeanFactoryUtils.getBeanClassLoader
    Replace the inline ConfigurableBeanFactory instanceof check and manual getBeanClassLoader call in isBeanPresent with a static BeanFactoryUtils.getBeanClassLoader helper. Adds the static import and simplifies class loader retrieval when resolving bean class names.

--- microsphere-spring-context/src/main/java/io/microsphere/spring/beans/BeanUtils.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/beans/BeanUtils.java
@@ -43,6 +43,7 @@ import static io.microsphere.invoke.MethodHandleUtils.handleInvokeExactFailure;
 import static io.microsphere.logging.LoggerFactory.getLogger;
 import static io.microsphere.spring.beans.factory.BeanFactoryUtils.asBeanDefinitionRegistry;
 import static io.microsphere.spring.beans.factory.BeanFactoryUtils.asConfigurableBeanFactory;
+import static io.microsphere.spring.beans.factory.BeanFactoryUtils.getBeanClassLoader;
 import static io.microsphere.spring.context.ApplicationContextUtils.asConfigurableApplicationContext;
 import static io.microsphere.spring.context.ApplicationContextUtils.getApplicationContextAwareProcessor;
 import static io.microsphere.util.ArrayUtils.isEmpty;
@@ -169,12 +170,7 @@ public abstract class BeanUtils implements Utils {
      */
     public static boolean isBeanPresent(@Nonnull ListableBeanFactory beanFactory, @Nonnull String beanClassName,
                                         boolean includingAncestors) {
-        ClassLoader classLoader = null;
-        if (beanFactory instanceof ConfigurableBeanFactory) {
-            ConfigurableBeanFactory configurableBeanFactory = (ConfigurableBeanFactory) beanFactory;
-            classLoader = configurableBeanFactory.getBeanClassLoader();
-        }
-
+        ClassLoader classLoader = getBeanClassLoader(beanFactory);
         Class beanClass = resolveClass(beanClassName, classLoader);
         if (beanClass == null) {
             return false;
