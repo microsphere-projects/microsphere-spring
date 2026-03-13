9e99501cb256d03eaaf53aae958b423543570712
Mercy <mercyblitz@gmail.com>
2026-03-13 20:00:35 +0800

    Add JUnit LoggingLevelsRule and Logback support
    Introduce a LoggingLevelsRule for JUnit4 that repeats test execution with multiple logging levels. Adds an abstract LoggingLevelsStatement, a LogbackLevelsStatement implementation, and a unit test (LoggingLevelsRuleTest). Registers the implementation via META-INF/spring.factories and uses SpringFactoriesLoader to discover statement implementations. Also adds logback-classic as an optional dependency in the module's pom (removed the duplicate test-scoped entry) and includes minor pom whitespace cleanup.

---
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/junit4/rule/logging/LogbackLevelsStatement.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.test.junit4.rule.logging;
+
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import static ch.qos.logback.classic.Level.toLevel;
+import static org.junit.runner.Description.createTestDescription;
+import static org.slf4j.LoggerFactory.getILoggerFactory;
+
+/**
+ * The {@link LoggingLevelsStatement} for logback.
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see LoggingLevelsStatement
+ * @since 1.0.0
+ */
+public class LogbackLevelsStatement extends LoggingLevelsStatement {
+
+    private static final LoggerContext loggerContext = (LoggerContext) getILoggerFactory();
+
+    public LogbackLevelsStatement(Statement next, Description description, String... levels) {
+        super(next, description, levels);
+    }
+
+    @Override
+    public void evaluate() throws Throwable {
+        Class<?> testClass = this.testClass;
+        Logger logger = getTargetLogger(testClass);
+        Level orignalLevel = logger.getLevel();
+        try {
+            for (String levelString : levels) {
+                description.addChild(createTestDescription(testClass, "with logging level - " + levelString));
+                Level level = toLevel(levelString);
+                logger.setLevel(level);
+                next.evaluate();
+            }
+        } finally {
+            // Reset original level
+            logger.setLevel(orignalLevel);
+        }
+    }
+
+    protected Logger getTargetLogger(Class<?> testClass) {
+        return loggerContext.getLogger(testClass.getPackage().getName());
+    }
+}
---
+++ microsphere-spring-test/src/main/java/io/microsphere/spring/test/junit4/rule/logging/LoggingLevelsRule.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.spring.test.junit4.rule.logging;
+
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.util.List;
+
+import static io.microsphere.util.ClassLoaderUtils.getClassLoader;
+import static io.microsphere.util.ClassLoaderUtils.resolveClass;
+import static io.microsphere.util.ClassUtils.newInstance;
+import static org.springframework.core.io.support.SpringFactoriesLoader.loadFactoryNames;
+
+/**
+ * The {@link TestRule} to iterate Logging levels
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see LoggingLevelsStatement
+ * @see TestRule
+ * @since 1.0.0
+ */
+public class LoggingLevelsRule implements TestRule {
+
+    private String[] levels;
+
+    protected LoggingLevelsRule(String... levels) {
+        this.levels = levels;
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        ClassLoader classLoader = getClassLoader(description.getTestClass());
+        List<String> loggingLevelsClassNames = loadFactoryNames(LoggingLevelsStatement.class, classLoader);
+        Statement targetStatement = base;
+        for (String loggingLevelsClassName : loggingLevelsClassNames) {
+            Class<?> loggingLevelsClass = resolveClass(loggingLevelsClassName, classLoader);
+            if (loggingLevelsClass != null) {
+                targetStatement = (Statement) newInstance(loggingLevelsClass, targetStatement, description, levels);
+            }
+        }
+        return targetStatement;
+    }
+
+    public static LoggingLevelsRule levels(String... levels) {
+        return new LoggingLevelsRule(levels);
+    }
+}
