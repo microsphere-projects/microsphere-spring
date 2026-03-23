4b4707a689a1d97d8b876e3837d76684a1729690
Mercy Ma <mercyblitz@gmail.com>
2026-03-24 03:28:38 +0800

    Centralize test logback and add test dependencies
    Move and consolidate test logging config and add shared test dependencies across modules.
    
    - Moved microsphere-spring-web/src/test/resources/logback-test.xml to microsphere-spring-test/src/main/resources/logback-test.xml and adjusted log levels (root -> INFO, added io.microsphere logger at TRACE).
    - Removed duplicate test Logback files from microsphere-spring-context, webflux, webmvc, and web modules.
    - Added io.github.microsphere-projects:microsphere-spring-test as a test dependency to spring-jdbc, spring-web, spring-webflux, and spring-webmvc poms to share test utilities/config.
    - Added sqlite-jdbc and logback-classic as test dependencies to microsphere-spring-jdbc pom.
    - Re-enabled and updated P6Spy-related tests: uncommented test classes and added LoggingLevelsClass annotations to configure test logging (including reference to P6DataSourceBeanPostProcessor).
    
    Purpose: consolidate common test logging configuration and share test helpers across modules, reduce duplication, and ensure required test dependencies for JDBC tests.

--- microsphere-spring-jdbc/src/test/java/io/microsphere/spring/jdbc/p6spy/annotation/EnableP6DataSourceTest.java
+++ microsphere-spring-jdbc/src/test/java/io/microsphere/spring/jdbc/p6spy/annotation/EnableP6DataSourceTest.java
@@ -1,68 +1,58 @@
-/// *
-// * Licensed to the Apache Software Foundation (ASF) under one or more
-// * contributor license agreements.  See the NOTICE file distributed with
-// * this work for additional information regarding copyright ownership.
-// * The ASF licenses this file to You under the Apache License, Version 2.0
-// * (the "License"); you may not use this file except in compliance with
-// * the License.  You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//package io.microsphere.spring.jdbc.p6spy.annotation;
-//
-//import com.p6spy.engine.common.ConnectionInformation;
-//import com.p6spy.engine.logging.LoggingEventListener;
-//import io.microsphere.spring.test.jdbc.embedded.EnableEmbeddedDatabase;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//import javax.sql.DataSource;
-//import java.sql.Connection;
-//import java.sql.SQLException;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertSame;
-//
-/// **
-// * {@link EnableP6DataSource} Test
-// *
-// * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
-// * @since 1.0.0
-// */
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = {
-//        EnableP6DataSourceTest.class
-//})
-//@EnableP6DataSource
-//@EnableEmbeddedDatabase(dataSource = "testDataSource")
-//class EnableP6DataSourceTest extends LoggingEventListener {
-//
-//    @Autowired
-//    @Qualifier("testDataSource")
-//    private DataSource dataSource;
-//
-//    @Test
-//    void test() throws Exception {
-//        Connection connection = dataSource.getConnection();
-//        assertNotNull(connection);
-//    }
-//
-//    @Override
-//    public void onBeforeGetConnection(ConnectionInformation connectionInformation) {
-//        try {
-//            assertSame(dataSource.unwrap(DataSource.class), connectionInformation.getDataSource());
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
+package io.microsphere.spring.jdbc.p6spy.annotation;
+
+import com.p6spy.engine.common.ConnectionInformation;
+import com.p6spy.engine.logging.LoggingEventListener;
+import io.microsphere.logging.test.jupiter.LoggingLevelsClass;
+import io.microsphere.spring.jdbc.p6spy.beans.factory.config.P6DataSourceBeanPostProcessor;
+import io.microsphere.spring.test.jdbc.embedded.EnableEmbeddedDatabase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * {@link EnableP6DataSource} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since 1.0.0
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        EnableP6DataSourceTest.class
+})
+@EnableP6DataSource
+@EnableEmbeddedDatabase(dataSource = "testDataSource")
+@LoggingLevelsClass(
+        loggingClasses = P6DataSourceBeanPostProcessor.class,
+        levels = {"TRACE", "INFO", "ERROR"}
+)
+class EnableP6DataSourceTest extends LoggingEventListener {
+
+    @Autowired
+    @Qualifier("testDataSource")
+    private DataSource dataSource;
+
+    @Test
+    void test() throws Exception {
+        Connection connection = dataSource.getConnection();
+        assertNotNull(connection);
+    }
+
+    @Override
+    public void onBeforeGetConnection(ConnectionInformation connectionInformation) {
+        try {
+            assertSame(dataSource.unwrap(DataSource.class), connectionInformation.getDataSource());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
