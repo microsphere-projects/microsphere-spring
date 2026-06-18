1d84ca060fe9ee550cfaf1b2b997db091da5516c
Mercy <mercyblitz@gmail.com>
2026-06-18 18:22:19 +0800

    Replace ImportSelector; use genericBeanDefinition
    Remove the unused ImportSelector import and update Javadoc to reference AnnotatedBeanCapableImportCandidate. Replace the project-specific genericBeanDefinitionBuilder utility with Spring's BeanDefinitionBuilder.genericBeanDefinition via a static import, and update the corresponding call site. Also adjust method signature formatting and imports to clean up the registrar implementation.

--- microsphere-spring-context/src/main/java/io/microsphere/spring/context/annotation/AutoRegistrationBeanRegistrar.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/context/annotation/AutoRegistrationBeanRegistrar.java
@@ -22,21 +22,20 @@ import io.microsphere.spring.core.annotation.ResolvablePlaceholderAnnotationAttr
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanNameGenerator;
-import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.List;
 
-import static io.microsphere.spring.beans.factory.support.BeanDefinitionBuilderUtils.genericBeanDefinitionBuilder;
 import static io.microsphere.spring.beans.factory.support.BeanRegistrar.registerBeanDefinition;
 import static io.microsphere.spring.constants.PropertyConstants.DEFAULT_AUTO_REGISTERED_VALUE;
 import static io.microsphere.spring.context.annotation.EnableAutoRegistrationBean.BEANS_AUTO_REGISTERED_PROEPRTY_NAME;
 import static io.microsphere.spring.context.config.AutoRegistrationBean.getAutoRegisteredPropertyName;
+import static org.springframework.beans.factory.support.BeanDefinitionBuilder.genericBeanDefinition;
 import static org.springframework.core.io.support.SpringFactoriesLoader.loadFactories;
 
 /**
- * {@link ImportSelector} class for {@link EnableAutoRegistrationBean}
+ * {@link AnnotatedBeanCapableImportCandidate} class for {@link EnableAutoRegistrationBean}
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see EnableAutoRegistrationBean
@@ -48,7 +47,8 @@ class AutoRegistrationBeanRegistrar extends AnnotatedBeanCapableImportCandidate<
 
     @Override
     protected void registerBeanDefinitions(AnnotationMetadata metadata, BeanDefinitionRegistry registry,
-                                           BeanNameGenerator importBeanNameGenerator, ResolvablePlaceholderAnnotationAttributes<EnableAutoRegistrationBean> annotationAttributes) {
+                                           BeanNameGenerator importBeanNameGenerator,
+                                           ResolvablePlaceholderAnnotationAttributes<EnableAutoRegistrationBean> annotationAttributes) {
         List<AutoRegistrationBean> autoRegistrationBeans = loadFactories(AutoRegistrationBean.class, super.classLoader);
         registerAutoRegisteredBeans(autoRegistrationBeans, registry);
     }
@@ -94,7 +94,7 @@ class AutoRegistrationBeanRegistrar extends AnnotatedBeanCapableImportCandidate<
 
         Class<AutoRegistrationBean> beanType = (Class<AutoRegistrationBean>) autoRegistrationBean.getBeanType();
         String scope = autoRegistrationBean.getScope();
-        BeanDefinitionBuilder beanDefinitionBuilder = genericBeanDefinitionBuilder(beanType, () -> autoRegistrationBean)
+        BeanDefinitionBuilder beanDefinitionBuilder = genericBeanDefinition(beanType, () -> autoRegistrationBean)
                 .setScope(scope);
 
         autoRegistrationBean.customize(beanDefinitionBuilder);
