7cdfbc6e408715bad890ce5efca68f56a60f543b
Mercy <mercyblitz@gmail.com>
2026-06-18 18:58:58 +0800

    Use genericBeanDefinitionBuilder helper
    Replace the direct call and static import of Spring's BeanDefinitionBuilder.genericBeanDefinition with the project's helper genericBeanDefinitionBuilder from BeanDefinitionBuilderUtils in AutoRegistrationBeanRegistrar. Updated the static import and the builder invocation; no other logic changes.

--- microsphere-spring-context/src/main/java/io/microsphere/spring/context/annotation/AutoRegistrationBeanRegistrar.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/context/annotation/AutoRegistrationBeanRegistrar.java
@@ -27,11 +27,11 @@ import org.springframework.core.type.AnnotationMetadata;
 
 import java.util.List;
 
+import static io.microsphere.spring.beans.factory.support.BeanDefinitionBuilderUtils.genericBeanDefinitionBuilder;
 import static io.microsphere.spring.beans.factory.support.BeanRegistrar.registerBeanDefinition;
 import static io.microsphere.spring.constants.PropertyConstants.DEFAULT_AUTO_REGISTERED_VALUE;
 import static io.microsphere.spring.context.annotation.EnableAutoRegistrationBean.BEANS_AUTO_REGISTERED_PROEPRTY_NAME;
 import static io.microsphere.spring.context.config.AutoRegistrationBean.getAutoRegisteredPropertyName;
-import static org.springframework.beans.factory.support.BeanDefinitionBuilder.genericBeanDefinition;
 import static org.springframework.core.io.support.SpringFactoriesLoader.loadFactories;
 
 /**
@@ -94,7 +94,7 @@ class AutoRegistrationBeanRegistrar extends AnnotatedBeanCapableImportCandidate<
 
         Class<AutoRegistrationBean> beanType = (Class<AutoRegistrationBean>) autoRegistrationBean.getBeanType();
         String scope = autoRegistrationBean.getScope();
-        BeanDefinitionBuilder beanDefinitionBuilder = genericBeanDefinition(beanType, () -> autoRegistrationBean)
+        BeanDefinitionBuilder beanDefinitionBuilder = genericBeanDefinitionBuilder(beanType, () -> autoRegistrationBean)
                 .setScope(scope);
 
         autoRegistrationBean.customize(beanDefinitionBuilder);
