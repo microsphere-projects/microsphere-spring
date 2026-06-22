157373c07365af4434f07f54398be8f51be86d57
Mercy Ma <mercyblitz@gmail.com>
2026-06-22 22:55:03 +0800

    Improve initializer Javadoc and add import
    Expand the Javadoc for ListenableConfigurableEnvironmentInitializer with detailed configuration notes, example usage, and registration instructions (property microsphere.spring.listenable-environment.enabled, default false). Also add an import for ListenableAutowireCandidateResolver. These changes improve discoverability and guidance for enabling and registering the listenable environment initializer.

--- microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/ListenableConfigurableEnvironmentInitializer.java
+++ microsphere-spring-context/src/main/java/io/microsphere/spring/core/env/ListenableConfigurableEnvironmentInitializer.java
@@ -18,6 +18,7 @@ package io.microsphere.spring.core.env;
 
 import io.microsphere.annotation.ConfigurationProperty;
 import io.microsphere.constants.PropertyConstants;
+import io.microsphere.spring.beans.factory.support.ListenableAutowireCandidateResolver;
 import io.microsphere.spring.context.ConfigurableApplicationContextInitializer;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -27,9 +28,35 @@ import static io.microsphere.spring.constants.PropertyConstants.MICROSPHERE_SPRI
 import static java.lang.Boolean.parseBoolean;
 
 /**
- * The Initializer of {@link ListenableConfigurableEnvironment} based on {@link ApplicationContextInitializer}
+ * An {@link ApplicationContextInitializer} implementation that initializes {@link ConfigurableEnvironment}
+ * with {@link ListenableConfigurableEnvironment} to enable listening for environment changes.
  *
- * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * <h3>Configuration Properties</h3>
+ * <ul>
+ *     <li>{@code microsphere.spring.listenable-environment.enabled} -
+ *         Whether to enable the {@link ListenableConfigurableEnvironment} (default: {@code false}).</li>
+ * </ul>
+ *
+ * <h3>Example Usage</h3>
+ * <p><strong>1. Configuration (application.properties):</strong></p>
+ * <pre>
+ * microsphere.spring.listenable-environment.enabled=true
+ * </pre>
+ *
+ * <p><strong>2. Registration (Spring Boot 2.x - spring.factories):</strong></p>
+ * <pre>{@code
+ * org.springframework.context.ApplicationContextInitializer=\
+ * io.microsphere.spring.beans.factory.support.ListenableAutowireCandidateResolverInitializer
+ * }</pre>
+ *
+ * <p><strong>3. Programmatic Registration:</strong></p>
+ * <pre>{@code
+ * SpringApplication application = new SpringApplication(MyApplication.class);
+ * application.addInitializers(new ListenableConfigurableEnvironmentInitializer());
+ * application.run(args);
+ * }</pre>
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see ListenableConfigurableEnvironment
  * @see EnvironmentListener
  * @see ProfileListener
